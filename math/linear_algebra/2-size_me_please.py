ecaede8be3a5f1d5c0b6a0f9ebc119f9abf000b3
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-09 11:08:06 +0200

    exercices d'algebre

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+Fonction contournant la methode shape de numpy
+"""
+
 
 def matrix_shape(matrix):
     dims = []
+
     while type(matrix) == list:
         dims.append(len(matrix))
         matrix = matrix[0]
