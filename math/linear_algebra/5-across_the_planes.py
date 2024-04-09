d085db57a9861234d1cae62bbfbc3fc45aee4480
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-09 13:29:02 +0200

    exercices d'algebre

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Fonction contournant la methode
+d'adition de matrice element par element
+de vecteurs de numpy
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    fonction de la task 5
+    """
+    row = []
+    sum = []
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    else:
+        for i in range(0, len(mat1[0])):
+            for j in range(0, len(mat1)):
+                row.append(mat1[i][j] + mat2[i][j])
+            sum.append(row)
+            row = []
+    return sum
