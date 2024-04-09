e69588cb3165d2f98c1c25603289d03609aca7e7
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-09 11:56:26 +0200

    exercices d'algebre

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Fonction contournant la methode
+transpose de numpy pour une matrice de dimension 2
+"""
+
+
+def matrix_transpose(matrix):
+    """ fonction pour remplir la task 2
+    """
+
+    row = []
+    mat = []
+    for i in range(0, len(matrix[0])):
+        for j in range(0, len(matrix)):
+            row.append(matrix[j][i])
+        mat.append(row)
+        row = []
+    return 0
