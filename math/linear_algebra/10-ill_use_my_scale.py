1f155d25d0e6844ff8ab7d4b4fbc0a0b47ec65d9
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-10 15:40:17 +0200

    exercices d'algebre

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""
+Task 1O, linear algebre project: I’ll Use My Scale
+"""
+
+
+def np_shape(matrix):
+    """ transpo a matrix without using np"""
+    return matrix.shape
