f4886c6454a7152d660531040da61347a82fa608
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-10 15:44:18 +0200

    exercices d'algebre

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""
+Task 12, linear algebre project: . Bracing The Elements
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """ assume multiple operatins on 2 elements """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
