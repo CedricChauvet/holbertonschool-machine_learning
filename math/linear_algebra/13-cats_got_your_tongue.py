b981dc49327ce1f98badd62c9b8af972d177525f
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-10 19:02:41 +0200

    exercices d'algebre

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import numpy as np
 """"
 Task 13, linear algebre project: . Cat's Got Your Tongue
 """
-
+import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
     """ assume concatenation on 2 matrices along a specific axe """
