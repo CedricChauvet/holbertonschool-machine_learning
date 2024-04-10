5482833352cbc8891f32c8b2edc02717040c964c
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-10 09:48:57 +0200

    exercices d'algebre

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -13,12 +13,12 @@ def add_matrices2D(mat1, mat2):
     """
     row = []
     sum = []
-    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+    if len(mat1) == 0:
         return None
     else:
-        for i in range(0, len(mat1[0])):
-            for j in range(0, len(mat1)):
-                row.append(mat1[i][j] + mat2[i][j])                
+        for i in range(0, len(mat1)):
+            for j in range(0, len(mat1[0])):
+                row.append(mat1[i][j] + mat2[i][j])
             sum.append(row)
             row = []
     return sum
