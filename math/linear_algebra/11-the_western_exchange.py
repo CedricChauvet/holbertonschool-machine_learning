252d6c0fd7de6f444cac686fe680463333b9e5c5
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-10 13:43:48 +0200

    exercices d'algebre

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""""
+Task 11, linear algebre project: The Western Exchange
+"""
+
+
+import numpy as np
+
+def np_transpose(matrix):
+    """ transpo a matrix using np"""
+    return np.transpose(matrix)*
