f7b29614c30bb54d1c78c5d856e51de7836e0578
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-10 19:00:23 +0200

    exercices d'algebre

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -7,7 +7,5 @@ Task 13, linear algebre project: . Cat's Got Your Tongue
 
 def np_cat(mat1, mat2, axis=0):
     """ assume concatenation on 2 matrices along a specific axe """
-    if axis == 0:
-        """ sum de 0 dim of the matrix,t the concatenate on this axe"""
-        mat_cat = np.concatenate((mat1, mat2), axis=axis)
-        return mat_cat
+    mat_cat = np.concatenate((mat1, mat2), axis=axis)
+    return mat_cat
