b3b74418d1edafcbbb54171e9bf5f14bc0bd31d7
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-11 14:51:45 +0200

    serie d exercices de la semaine 1

---
+++ math/plotting/0-line.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Task 0, project plotting: Line Graph
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(np.arange(0, 11), y, 'r')
+    plt.xlim((0, 10))
+    plt.show()
