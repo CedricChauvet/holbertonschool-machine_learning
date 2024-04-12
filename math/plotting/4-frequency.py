539d81e53d715b046782b1d2f4114e3313c758ab
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-12 11:19:44 +0200

    serie d exercices de la semaine 1

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def frequency():
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.hist(student_grades.astype("int32"),bins = [10,20,30,40,50,60,70,80,90,100], edgecolor="black")
+    plt.xlim(0,100)
+    plt.xticks(np.arange(0,100,10))
+    plt.yticks(np.arange(0,31,5))
+    
+    plt.show()
