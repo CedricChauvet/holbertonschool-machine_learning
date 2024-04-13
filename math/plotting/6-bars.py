2f3febb4358182c6c112ed5b92a846e1709f52cd
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-13 21:14:40 +0200

    dernier commitsur le plotting

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -6,19 +6,15 @@ def bars():
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4,3))
     plt.figure(figsize=(6.4, 4.8))
-    colors = ["red", "yellow", "orange", "#ffe5b4"]
+    colors = ["red", "yellow", "#ff8000", "#ffe5b4"]
     
-    apples = (fruit[0,0],fruit[1,0],fruit[2,0])
-    bananas = (fruit[0,1],fruit[1,1],fruit[2,1])
-    oranges = (fruit[0,2],fruit[1,2],fruit[2,2])
-    peaches = (fruit[0,3],fruit[1,3],fruit[2,3])
     
-    persons=("Farrah","Fred","Felicia" )
+    persons=("Farrah", "Fred", "Felicia")
     weight_counts = {
-    "apples": np.array([fruit[0,0],fruit[1,0],fruit[2,0]]),
-    "bananas": np.array([fruit[0,1],fruit[1,1],fruit[2,1]]), 
-    "oranges": np.array([fruit[0,2],fruit[1,2],fruit[2,2]]),
-    "peaches": np.array([fruit[0,3],fruit[1,3],fruit[2,3]]),
+    "apples": np.array([fruit[0,0],fruit[0,1],fruit[0,2]]),
+    "bananas": np.array([fruit[1,0],fruit[1,1],fruit[1,2]]), 
+    "oranges": np.array([fruit[2,0],fruit[2,1],fruit[2,2]]),
+    "peaches": np.array([fruit[3,0],fruit[3,1],fruit[3,2]])
     }
     bottom = np.zeros(3)
     i=0
