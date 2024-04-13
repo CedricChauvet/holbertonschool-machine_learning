8f588e785e8e48c4b2f28a7af79a84715ab37dfa
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-13 20:41:33 +0200

    dernier commitsur le plotting

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -15,7 +15,7 @@ def frequency():
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    plt.hist(student_grades,bins = [10,20,30,40,50,60,70,80,90,100], edgecolor="black")
+    plt.hist(student_grades,bins = [0,10,20,30,40,50,60,70,80,90,100], edgecolor="black")
     plt.xlim(0,100)
     plt.xticks(np.arange(0,101,10))
     plt.yticks(np.arange(0,31,5))
