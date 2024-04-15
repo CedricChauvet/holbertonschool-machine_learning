81ce17cb33024660d5afcd947fe5b32969375632
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-15 12:35:51 +0200

    premier jour, 14 avril 24

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,7 +9,7 @@ and those decisions are determined by our priorities
 def summation_i_squared(n):
     """this is a documentation"""
 
-    if type(n) is not int n == None:
+    if type(n) is not int or n == None:
         return None
     else:
         return int(n * (n + 1) * (2 * n + 1) / 6)
