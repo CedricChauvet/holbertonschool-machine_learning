1056396c8df5290a1dd70382c2a3b8f357657b49
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-15 13:26:35 +0200

    premier jour, 14 avril 24

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Task 17 project calculus: Integrate
+"""
+
+
+def poly_integral(poly, C=0):
+    poly_int=[C]
+    
+    if poly == None:
+        return None
+    for i poly:
+        if i is not (float, int)
+            return None
+    
+    for i in range(len(poly)):
+        
+        if poly[i]/(i + 1) % 1 <= 0.0:
+            poly_int.append(int(poly[i]/(i + 1)))
+        
+        elif isinstance( poly[i]/(i + 1), float):
+            poly_int.append(float(poly[i]/(i + 1)))
+    return poly_int
