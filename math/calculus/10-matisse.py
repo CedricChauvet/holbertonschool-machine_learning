303393dd5c2ddbcd399cff5d521cd978e0ca2419
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-15 11:25:00 +0200

    premier jour, 14 avril 24

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -7,12 +7,13 @@ task 10, project calculus: Derive happiness in oneself from a good day's work
 def poly_derivative(poly):
     """ This is a documentation """
     list2 = []
-
-    for elm in poly:
-        if type(elm) is not int:
+    for i in range (len(poly)):
+        if type(poly[i]) is not int:
             return None
+
     if len(poly) == 1:
         return [0]
     for i in range(0, len(poly) - 1):
+
         list2.append(poly[i+1]*(i+1))
     return list2
