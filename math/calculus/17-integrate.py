784c62047a0a2481e01e41f9c6013c69b3204301
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-15 15:48:17 +0200

    premier jour, 14 avril 24

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -9,24 +9,18 @@ def poly_integral(poly, C=0):
     if type(C) not in (int, float):
         return None
     if type(poly) is not list or poly == []:
-        return None 
+        return None
     for i in poly:
-        if type(i) not in (int, float) :
+        if type(i) not in (int, float):
             return None
-    
+    # Create the integrale matrix, append C
     poly_int = [C]
     for i in range(len(poly)):
-        if isinstance(i,int) or isinstance(i,float):
-            if poly[i]/(i + 1) % 1 <= 0.0:
-                poly_int.append(int(poly[i]/(i + 1)))
-        
-            elif isinstance( poly[i]/(i + 1), float):
-                poly_int.append(float(poly[i]/(i + 1)))
-        else:
-            return False
-    while poly_int[-1] == 0 and len(poly_int)>1:
+        if poly[i]/(i + 1) % 1 <= 0.0:
+            poly_int.append(int(poly[i]/(i + 1)))
+        elif isinstance(poly[i]/(i + 1), float):
+            poly_int.append(float(poly[i]/(i + 1)))
+    # erase the 0 on the left of the array
+    while poly_int[-1] == 0 and len(poly_int) > 1:
         poly_int.pop(-1)
-
-
-
     return poly_int
