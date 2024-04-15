8cc7bf334991366228776c1d08ddd9277566315d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-15 15:29:50 +0200

    premier jour, 14 avril 24

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -13,21 +13,7 @@ def poly_integral(poly, C=0):
         if type(i) not in (int, float) :
             return None
     
-    
-    
-    poly_reduct = poly[::-1]
-    if poly_reduct[0] == 0:
-        poly_reduct.pop(0)
-    poly = poly_reduct[::-1]
-    
-    if poly == []:
-        return poly_int
-    
-    for i in poly:
-        if type(i) not in (float, int):
-            return None
-    
-    poly_int=[C]
+    poly_int = [C]
     for i in range(len(poly)):
         
         if poly[i]/(i + 1) % 1 <= 0.0:
@@ -35,5 +21,10 @@ def poly_integral(poly, C=0):
         
         elif isinstance( poly[i]/(i + 1), float):
             poly_int.append(float(poly[i]/(i + 1)))
+
+    while poly_int[-1] == 0:
+        poly_int.pop(-1)
+
+
 
     return poly_int
