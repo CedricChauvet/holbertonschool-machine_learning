aa5af02ee6323a60de4d07c3fce2e1d35ca32875
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-23 11:58:21 +0200

    realisation de la task 1, valeaurs privées

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+documentation création d'une classe neuro task 1: Privatize Neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """putting private instance where @perperties are getter """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
