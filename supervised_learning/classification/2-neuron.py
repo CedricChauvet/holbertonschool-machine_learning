6893e6de9d900cb976b289f48f2e9371400c04bc
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-23 13:51:10 +0200

    added a setter and method forward_prop

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -42,4 +42,4 @@ class Neuron:
         forward_var = np.array(np.dot(X.T, self.__W.T) + self.__b)
         activation = np.array(1 / (1 + np.exp(-forward_var))).T
         self.__A = activation
-        return self.__A
+        return self.A
