05aeb63e421975f21ce09c637b8678b372ee275a
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-24 14:45:18 +0200

    ajout d'une  derniere evaluation

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -112,14 +112,15 @@ class Neuron:
             raise TypeError("iterations must be an integer")
         elif iterations < 0:
             raise ValueError("iterations must be a positive integer")
-            
+
         if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         elif alpha < 0:
             raise ValueError("alpha must be positive")
-            
+
         for i in range(iterations):
-            A= self.forward_prop(X)
-            self.gradient_descent( X, Y, A, alpha=alpha)
-            cost = self.cost(Y,A)
-        return A,cost
+            print("A", self.A)
+            A, cost = self.evaluate(X, Y)
+            self.gradient_descent(X, Y, self.__A, alpha=alpha)
+        A, cost = self.evaluate(X, Y)
+        return A, cost
