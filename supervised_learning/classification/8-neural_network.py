04d6dd730971a3e998ae2bdeb8477345197acf5e
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-25 12:02:04 +0200

    premier commit sur neural network

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+First task with a new class: NeuralNetwork
+Write a class NeuralNetwork that defines a neural network with one
+hidden layer performing binary classification
+
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+
+    def __init__(self, nx, nodes):
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        elif nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nodes = nodes
+
+        self.W1 = np.random.normal(size=(1, nx))
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
