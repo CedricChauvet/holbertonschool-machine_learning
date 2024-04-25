2c6c4554f3d5a5c0e2af8b78d93476435ab704ea
CedricChauvet <chauvetc.ujf@gmail.com>
2024-04-25 12:07:28 +0200

    premier commit sur neural network

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -25,7 +25,7 @@ class NeuralNetwork:
         self.nodes = nodes
 
         self.W1 = np.random.normal(size=(1, nx))
-        self.b1 = np.zeros(nodes)
+        self.b1 = np.zeros(nx)
         self.A1 = 0
         self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = 0
