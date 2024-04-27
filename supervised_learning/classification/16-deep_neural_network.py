9e35d5b7ac1a139ff1a198b50355c891e02d6fa7
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-27 19:18:03 +0200

    aide pour démarrer

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -6,14 +6,14 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """ define a new class"""
-    def __init__(self, nx, layers=[]):
+    def __init__(self, nx, layers):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         elif nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
 
-        if type(layers) is not list:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.layers = layers
