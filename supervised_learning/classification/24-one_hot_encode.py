372adba506bc5a08eca22481b5655d7f766b577b
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-28 18:55:40 +0200

    intermezzo, one hot encode

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-
-import numpy as np
-
 """Write a function def one_hot_encode(Y, classes): that converts
  a numeric label vector into a one-hot matrix:
 """
+import numpy as np
 
 
 def one_hot_encode(Y, classes):
     """transfom a numeric label vector into a one-hot matrix Ho is
      an np array of (cl,m), cl is the maximum number of classes found in Y,
     m the number of inputs"""
-   
-    if type(Y) is not np.ndarray or type(classes) is not int or classes < 2 or classes < np.max(Y):
+
+    if type(Y) is not np.ndarray or type(classes) is not int or classes < 2 \
+            or classes < np.max(Y):
         return None
 
     Ho = np.zeros((classes, Y.shape[0]))
