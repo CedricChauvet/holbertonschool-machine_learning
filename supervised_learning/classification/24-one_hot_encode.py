43d85a7219da13867285f914ce092ce642fb2687
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-04-28 18:35:54 +0200

    intermezzo, one hot encode

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+"""Write a function def one_hot_encode(Y, classes): that converts
+ a numeric label vector into a one-hot matrix:
+"""
+
+
+def one_hot_encode(Y, classes):
+    """transfom a numeric label vector into a one-hot matrix Ho is
+     an np array of (cl,m), cl is the maximum number of classes found in Y,
+    m the number of inputs"""
+    Ho = np.zeros((10, len(Y)))
+
+    for i, j in enumerate(Y):
+        Ho[j, i] = 1
+    return Ho
