107cd6db66e1446f2158694e4d5899e3a1b7a16d
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-06 19:48:25 +0200

    creation d'un modele, attnetion aux parametres lambtha (regularisation) et le dropout qui donne un summary different de d'habitude

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -8,10 +8,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     # define the keras model
     model = K.Sequential()
+    input_shape = (nx,)
     for lay, act in zip(layers, activations):
         model.add(K.layers.Dense(lay, activation=act, input_shape=(nx,),
                   kernel_regularizer=K.regularizers.l2(lambtha)))
         model.add(K.layers.Dropout(1 - keep_prob))
-        input_shape = (nx,)
+        
 
     return model
