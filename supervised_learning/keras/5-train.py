fa454e4de3735333e23d04d18efa491ac2052392
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-07 19:15:53 +0200

    be careful, wrong values fixed

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -4,19 +4,21 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, verbose=True, shuffle=False):
     """
     this is the task 5, train a model adding a validation data
     """
 
     if validation_data is None:
-        history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
-                          verbose=False, shuffle=shuffle, validation_data =validation_data)
-        
+        history = network.fit(data, labels, epochs=epochs,
+                              batch_size=batch_size,
+                              verbose=False, shuffle=shuffle,
+                              validation_data=validation_data)
 
     else:
-        
-        history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
-                          verbose=verbose, shuffle=shuffle, validation_data =validation_data)
+        history = network.fit(data, labels,
+                              epochs=epochs, batch_size=batch_size,
+                              verbose=verbose, shuffle=shuffle,
+                              validation_data=validation_data)
     return history
-        
