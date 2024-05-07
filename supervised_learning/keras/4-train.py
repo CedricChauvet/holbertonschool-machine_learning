b71960ccbfcb82218f3e9f1377357050a2215889
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-07 11:58:25 +0200

    fitting a model then return its History

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -4,7 +4,7 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose= "auto", shuffle=False):
+def train_model(network, data, labels, batch_size, epochs, verbose= False, shuffle=False):
     """
     this is the task 4, train a model
     """
