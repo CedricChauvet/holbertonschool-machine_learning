50dcb1f41bf8d68dfd6b6b646dfedba604c04d94
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-07 10:03:50 +0200

    shorter version of classification, without numpy

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -2,21 +2,15 @@
 """Write a function def one_hot_encode(Y, classes): that converts
  a numeric label vector into a one-hot matrix:
 """
-import numpy as np
 
 
-def one_hot(Y):
+def one_hot(Y, classes=None):
     """transfom a numeric label vector into a one-hot matrix Ho is
      an np array of (cl,m), cl is the maximum number of classes found in Y,
     m the number of inputs"""
-
-    classes = Y.shape[0]
-
-    if type(Y) is not np.ndarray or type(classes) is not int or classes < 2 \
-            or classes < np.max(Y):
-        return None
-
-    Ho = np.zeros((classes, Y.shape[0]))
-    for i, j in enumerate(Y):
-        Ho[j, i] = 1
+    
+    classes = len(Y)
+    Ho = [[0] * len(Y) for _ in range(classes)]
+    for j, i in enumerate(Y):
+        Ho[j][i] = 1
     return Ho
