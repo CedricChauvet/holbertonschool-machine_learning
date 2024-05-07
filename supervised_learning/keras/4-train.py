28edb3563a6c4cf81dd6ea19c6741ae22fac4192
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-07 12:56:27 +0200

    add a verbose False method

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -5,13 +5,14 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-                verbose=True, shuffle=False):
+                verbose=False, shuffle=False):
     """
     this is the task 4, train a model
     """
 
     history = network.fit(data, labels, epochs=epochs,
                 batch_size=batch_size, verbose=verbose, shuffle=shuffle)
+    
     if verbose is False:
         valuesloss = [float("{:.5f}".format(x)) for x in history.history["loss"]]
         valueaccurate = [float("{:.5f}".format(x)) for x in history.history["accuracy"]]
