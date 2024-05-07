22915229c85923cc4a7e4f3d1962d33bcc2d7773
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-07 12:21:36 +0200

    fitting a model then return its History

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -12,5 +12,6 @@ def train_model(network, data, labels, batch_size, epochs,
 
     history = network.fit(data, labels, epochs=epochs,
                 batch_size=batch_size, verbose=verbose, shuffle=shuffle)
-    # print(history.history.items())
-    return history.history.items()
+    if verbose is False:
+        print(history.history.items())
+    return history
