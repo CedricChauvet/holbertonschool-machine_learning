b85a95415837327069bc430d951c0826c27ff91c
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-08 22:08:37 +0200

    create 2 placeholders named x and y, put None to fit with any dimensions

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -7,9 +7,9 @@ import tensorflow.compat.v1 as tf
 
 def create_placeholders(nx, classes):
     """
-    Zero task: 0. Placeholders 
+    Zero task: 0. Placeholders
     """
-    x = tf.placeholder(tf.float32, shape=(None,nx), name="x")
-    y = tf.placeholder(tf.float32, shape=(None,classes), name="y")
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
 
     return x, y
