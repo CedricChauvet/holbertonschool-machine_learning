c3f198d8f134accaa796433e2f956380088fa00e
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-09 19:19:05 +0200

    trying with documentatoin found in holberton tensoflow project

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -7,6 +7,14 @@ tf.disable_eager_execution()
 
 
 def calculate_loss(y, y_pred):
+    """
+    Write the function def calculate_loss(y, y_pred): that calculates the softmax cross-entropy loss of a prediction:
+
+    y is a placeholder for the labels of the input data
+    y_pred is a tensor containing the network’s predictions
+    Returns: a tensor containing the loss of the prediction
+    """
     loss = tf.compat.v1.losses.softmax_cross_entropy(
            onehot_labels=y, logits=y_pred)
+
     return loss
