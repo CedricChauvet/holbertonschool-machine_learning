b67ea9cbb442c73bbe8669d5638e8635958f96ce
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-09 19:02:57 +0200

    solution elaborée avec l'aide de chatgpt

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-2 task on tensoflow project
+3rd task on tensoflow project
 """
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
 
 def calculate_accuracy(Y, y_pred):
+    """ créate a function that compute the accuracy between
+    a placeholder Y (input of data) and prediction y_pred
+    """
     # Créer un tenseur de booléens pour les prédictions correctes
-    correct_predictions = tf.equal(tf.argmax(Y, axis=1), tf.argmax(y_pred, axis=1))
-    
+    correct_predictions = tf.equal(Y, y_pred)
+
     # Convertir les booléens en floats (0.0 pour False, 1.0 pour True)
     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
-    
+
     return accuracy
