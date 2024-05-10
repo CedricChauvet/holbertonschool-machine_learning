c611b10d900aab80298bf70e900e4e75d01f5a9e
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-10 19:07:03 +0200

    pld was helpfull, look at sess = tf.Sessions()

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -49,3 +49,4 @@ def train(X_train, Y_train, X_valid, Y_valid,layer_sizes, activations, alpha, it
 
     # Save the variables to disk
     save_path = saver.save(sess, save_path)
+    return save_path
