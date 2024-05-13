1c967cd4b4f29b156c78e281c1d5959a6a914c89
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-13 16:00:56 +0200

    alright, reshape

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -11,8 +11,11 @@ def shuffle_data(X, Y):
     Write the functionthat shuffles the data points
     in two matrices the same way:
     """
-
-    X_sh = np.random.permutation(X)
-    Y_sh = np.random.permutation(Y)
+    m = X.shape[0]
+    nx = X.shape[1]
+    ny = Y.shape[1]
+    # print("m , nx , ny", m ,nx ,ny)
+    X_sh = np.random.permutation(X).reshape((m, nx))
+    Y_sh = np.random.permutation(Y).reshape((m, ny))
 
     return X_sh, Y_sh
