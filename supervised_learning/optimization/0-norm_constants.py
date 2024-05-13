f5f7080c91dd0c82ad15e95db294c0f4729e98e2
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-13 15:22:22 +0200

    for this exercice a version using lists

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Optimization project
+
+"""
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    Do the mean and standard deviation of each feature (here a, b, c)
+    the normalization (standardization) constants of a matrix
+    """
+    # n is the length of element to process
+    n = X.shape[0]
+
+    # using list to compute
+    m = []
+    s = []
+    print("n", n)
+    for elmt in X.T:  # beware of transpose
+
+        # this is the mean
+        m.append(np.sum(elmt)/n)
+        # The standard deviation,
+        # usually called "Population Standard Deviation"
+        s.append(np.sqrt(np.sum(1 / n * np.sum((elmt - m[-1]) ** 2))))
+    return m, s
