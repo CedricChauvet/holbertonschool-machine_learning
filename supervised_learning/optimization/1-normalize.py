4a084fcf0d57c6d7f9a509d4aa9db6ea39bf5c1e
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-13 15:44:57 +0200

    maybe an issue about number of decimal

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -11,7 +11,9 @@ def normalize(X, m, s):
     task 1: Normalize
     this function normalizes (standardizes) a matrix
     """
-
+    # we center the data on the origin
     mean = X - m
+    # stretching!
     stand = mean / s
+
     return stand
