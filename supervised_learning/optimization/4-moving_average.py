6cde0b664f0ce04075d2eb6aa95b19bfd1c3716b
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-14 11:30:52 +0200

    index 0 poped and ok

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Optimization project
+by Ced
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def moving_average(data, beta):
+    """
+    calculates the weighted moving average
+    of a data set:
+    """
+
+    v_avg = [0]
+    bias = []
+    result = [0]
+    for i in range(1, len(data) + 1):
+        bias.append(1 - (beta ** i))
+
+        v_avg.append(beta * v_avg[-1] + (1 - beta) * (data[i-1]))
+
+        result.append(v_avg[-1] / bias[-1])
+    result.pop(0)
+
+    return result
