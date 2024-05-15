a294d70c2c3dbb6199d7cc46f2d4e5e7d751df18
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-15 15:28:32 +0200

    see https://medium.com/analytics-vidhya/why-use-the-momentum-optimizer-with-minimal-code-example-8f5d93c33a53

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Optimization project
+by Ced
+"""
+import numpy as np
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    hat sets up the gradient descent with momentum optimization
+    algorithm in TensorFlow
+    return the optimizer
+    """
+    sgd = tf.keras.optimizers.SGD(lr=alpha, momentum=beta1)
+
+    return sgd
