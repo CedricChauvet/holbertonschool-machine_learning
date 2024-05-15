6e8be4b3a12726665a792ce20d8b54e07594a9bd
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-15 16:07:49 +0200

    bad values but...

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 """
 Optimization project
-Task 8 RMSProp upgrader with tensorflow
+Task 8 RMSProp upgraded with tensorflow
 by Ced
 """
 import tensorflow as tf
 
 
 def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    sets up the RMSProp optimization algorithm in TensorFlow
+
+    alpha is the learning rate
+    beta2 is the RMSProp weight (Discounting factor)
+    epsilon is a small number to avoid division by zero
+    Returns: optimizer
+    """
+
     optimizer = tf.keras.optimizers.RMSprop(
-    learning_rate=alpha,
-    rho=beta2,
-    epsilon=epsilon,
-    name='rmsprop',
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon,
+        name='rmsprop',
     )
+
     return optimizer
