ba8e662af0ef92208592cc93c99fa3bb99f0c556
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-15 16:58:20 +0200

    see https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/Adam

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -9,7 +9,8 @@ import tensorflow as tf
 
 def create_Adam_op(alpha, beta1, beta2, epsilon):
     """
-    Write the function def create_Adam_op(alpha, beta1, beta2, epsilon):that sets up the Adam optimization algorithm in TensorFlow:
+    Write the function def create_Adam_op(alpha, beta1, beta2, epsilon):
+    that sets up the Adam optimization algorithm in TensorFlow:
 
     alpha is the learning rate
     beta1 is the weight used for the first moment
@@ -20,11 +21,11 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
     """
 
     optim = tf.keras.optimizers.Adam(
-    learning_rate=alpha,
-    beta_1=beta1,
-    beta_2=beta2,
-    epsilon=epsilon,
-    name='adam',
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon,
+        name='adam',
     )
+
     return optim
-    
