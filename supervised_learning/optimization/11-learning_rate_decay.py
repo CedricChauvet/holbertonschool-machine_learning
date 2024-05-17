48fe93807f385ca12745aa83be44d0377aa0c0f8
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-17 11:51:46 +0200

    careful on the div entiere // on global_step

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -8,17 +8,21 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
-    Write the function def learning_rate_decay(alpha, decay_rate, global_step, decay_step): that updates the learning rate using inverse time decay in numpy:
+    Write the function def learning_rate_decay
+    (alpha, decay_rate, global_step, decay_step):
+    that updates the learning rate using inverse time decay in numpy:
 
     alpha is the original learning rate
-    decay_rate is the weight used to determine the rate at which alpha will decay
+    decay_rate is the weight used to determine the rate at which alpha will
+    decay
     global_step is the number of passes of gradient descent that have elapsed
-    decay_step is the number of passes of gradient descent that should occur before alpha is decayed further
+    decay_step is the number of passes of gradient descent that should occur
+    before alpha is decayed further
     the learning rate decay should occur in a stepwise fashion
 
     Returns: the updated value for alpha
     """
-    
-    lr = alpha / (1 + (decay_rate * (global_step  // decay_step)))
+
+    lr = alpha / (1 + (decay_rate * (global_step // decay_step)))
 
     return lr
