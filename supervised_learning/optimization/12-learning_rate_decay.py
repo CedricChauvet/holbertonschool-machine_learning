c3682efab77313d89fff861f70a65a8c6cab4889
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-17 13:13:57 +0200

    tensorflow learning rate

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -7,11 +7,12 @@ by Ced
 import tensorflow as tf
 
 
-def learning_rate_decay(alpha, decay_rate, decay_step):
+def learning_rate_decay(alpha, decay_rate1, decay_step1):
     optim = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=alpha,
-        decay_steps=decay_step,
-        decay_rate=decay_rate,
+        decay_steps=decay_step1,
+        decay_rate=decay_rate1,
+        staircase=True,
         name='ExponentialDecay'
 )
     return optim
