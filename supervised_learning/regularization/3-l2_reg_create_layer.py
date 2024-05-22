test wrong results

supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -10,10 +10,12 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activ, lambtha):
     
-    
-    tf.keras.regularizers.L2(
-    l2=lambtha
-)
+
+
     L2_layer = tf.keras.layers.Dense(n,activation=activ,
-            kernel_regularizer='l2')(prev)
+           kernel_regularizer=tf.keras.regularizers.L2(
+    l2=lambtha))(prev)
     return L2_layer    
+
+
+#kernel_regularizer='l2', bias_regularizer='l2' activity_regularizer='l2'