76cfbd68d66b8d881d632094e8c5dcbcce6f4d0d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-22 16:04:59 +0200

    test wrong results

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -14,7 +14,6 @@ def l2_reg_create_layer(prev, n, activ, lambtha):
 
     L2_layer = tf.keras.layers.Dense(units=n,activation=activ,
            kernel_regularizer=tf.keras.regularizers.L2(
-    l2=lambtha), bias_regularizer=tf.keras.regularizers.L2(
     l2=lambtha))(prev)
     
     return L2_layer    
