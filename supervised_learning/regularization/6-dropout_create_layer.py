7c3f10720a647bd11fc5931e276ecc1eb592b869
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-23 20:44:04 +0200

    creates a class(layer) in order to call training

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Regularization project
+by Ced
+"""
+import tensorflow as tf
+
+
+class CustomDropoutLayer(tf.keras.layers.Layer):
+    """
+    creating a class in order to call training
+    """
+    def __init__(self, rate, train):
+        super(CustomDropoutLayer, self).__init__()
+        self.rate = rate
+        self.dropout = tf.keras.layers.Dropout(rate)
+        self.train = train
+
+    def call(self, inputs, training):
+        return self.dropout(inputs, training=self.train)
+
+
+def dropout_create_layer(prev, n, activ, keep_prob, training=True):
+    """
+    create a layer with dropout
+    """
+    x = tf.keras.layers.Dense(n, activation=activ)(prev)
+
+    # Custom Dropout layer with explicit training argument
+    x = CustomDropoutLayer(1 - keep_prob, training)(x)
+    return x
