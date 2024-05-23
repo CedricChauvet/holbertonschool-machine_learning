66c3aeac007cade01ea0cf14a5b6499e075e7fa5
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-23 19:28:19 +0200

    issue corrected

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -7,7 +7,9 @@ import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """Calculates one pass of gradient descent on a DNN"""
+    """Calculates one pass of gradient descent on a DNN 
+    with dropout regularization
+    """
     # taille des exemples
     m = Y.shape[1]
 
@@ -18,24 +20,23 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
 
     for i in reversed(range(1, L + 1)):
         A_prev = cache["A" + str(i - 1)] if i > 1 else cache["A0"]
-        
+
         # mask dropout
         cache[f"D{i}"] = np.random.rand(
             A_prev.shape[0],
             A_prev.shape[1]) < keep_prob
         cache[f"D{i}"] = cache[f"D{i}"].astype(int)
 
-
         W = weights["W" + str(i)]
         b = weights["b" + str(i)]
 
         # Compute gradients
-        dW = (1 / m) * np.dot(dZ, A_prev.T) 
+        dW = (1 / m) * np.dot(dZ, A_prev.T)
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
         if i > 1:
             # derivative of tanh activation in the second term
-            dA_prev = np.dot(W.T, dZ) 
+            dA_prev = np.dot(W.T, dZ)
             dA_prev *= cache[f"D{i-1}"]
             dA_prev /= keep_prob
             dZ = (1 - A_prev ** 2) * dA_prev
