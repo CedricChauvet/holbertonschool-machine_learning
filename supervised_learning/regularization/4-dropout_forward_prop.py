ba592fa68b54bbcabcbf2b889294e01baeda74c3
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-23 18:21:38 +0200

    dropout see dropout regularization on deeplearningIA

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -8,24 +8,27 @@ import numpy as np
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
+    Forward Propagation with Dropout
     """
-    
-    
+
     cache = dict()
-    for i in range(L ):
+    for i in range(L):
         if i == 0:
             cache['A0'] = X
         else:
             z = np.dot(weights[f"W{i}"], cache[f"A{i-1}"]
-                ) + weights[f"b{i}"]
-            
+                       ) + weights[f"b{i}"]
+
             tanh_z = np.tanh(z)
             cache[f"A{i}"] = tanh_z
-            print(f"shape W{i}",weights[f"W{i}"].shape)
-            
-            x=np.random.randint(100, size=())
-    
-    cache[f"A{L}"] = np.dot(weights[f"W{L}"], cache[f"A{L-1}"]) + weights[f"b{L}"]
-    
-    
+
+            dropout = np.random.rand(cache[f"A{i}"].shape[0],
+                                     cache[f"A{i}"].shape[1]) < keep_prob
+
+            cache[f"A{i}"] *= dropout
+            cache[f"A{i}"] /= keep_prob
+
+    cache[f"A{L}"] = np.dot(weights[f"W{L}"],
+                            cache[f"A{L-1}"]) + weights[f"b{L}"]
+
     return cache
