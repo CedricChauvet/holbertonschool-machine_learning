dae5a0c51158935265c987efbb435bfd818a41c3
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-24 11:15:48 +0200

    last task, understanding early stopping

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -7,15 +7,19 @@ by Ced
 
 def early_stopping(cost, opt_cost, threshold, patience, count):
     """
-    doc
+    task 7 early stopping
+
+    cost is the current validation cost of the neural network
+    opt_cost is the lowest recorded validation cost of the neural network
+    threshold is the threshold used for early stopping
+    patience is the patience count used for early stopping
+    count is the count of how long the threshold has not been met
     """
-    if (opt_cost - cost) > threshold:
+    if(opt_cost - cost) > threshold:
         return (False, 0)
-    
-    elif (opt_cost - cost) <= threshold:
-        if count < patience -1:
-            return (False, count +1)
-        if count == patience -1:
+
+    elif(opt_cost - cost) <= threshold:
+        if count < patience - 1:
+            return (False, count + 1)
+        if count == patience - 1:
             return(True, patience)
-        
-        
