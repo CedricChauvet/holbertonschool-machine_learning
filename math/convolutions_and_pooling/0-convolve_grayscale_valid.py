4bcbb830cd33fb22c824fe663d0a0ab55d7fdc88
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-28 11:37:24 +0200

    convulotion method

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -7,27 +7,40 @@ import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    #print("kern shape",kernel.shape[0])
+    """
+    performs a valid convolution on grayscale images:
+
+    images is a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
+
+    m is the number of images
+    h is the height in pixels of the images
+    w is the width in pixels of the images
+
+    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel
+    for the convolution
+    kh is the height of the kernel
+    kw is the width of the kernel
+    """
+
+    # print("kern shape",kernel.shape[0])
     kw = kernel.shape[0]
     # m is the number of images,  h is the height of  an image, w the width
     m = images.shape[0]
     h = images.shape[1]
     w = images.shape[2]
-    
-    
+
     # shape of the output
     conv_h = h - (kw - 1)
     conv_w = w - (kw - 1)
-    
-    
+
     # creating array for the output
     conv_image = np.zeros((m, conv_h, conv_w))
-    
-    
+
     for i in range(conv_h):
         for j in range(conv_w):
-            crop = images[:,i: i+kw, j:j+kw]
+            crop = images[:, i: i + kw, j:j + kw]
             # beware to not sum the m number of images
-            conv_image[:,i,j]= np.sum(crop[:] * kernel, axis=(1, 2))
-    
+            conv_image[:, i, j] = np.sum(crop[:] * kernel, axis=(1, 2))
+
     return conv_image
