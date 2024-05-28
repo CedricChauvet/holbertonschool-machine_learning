2b8c361fbf356531a69f75c65968ea9a8c4d7a04
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-28 11:57:34 +0200

    adding a padding in order to get the same shape like the exemple

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Convolution and pooling project
+by Ced
+"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """
+    performs a same convolution on grayscale images with padding
+    """
+
+    kw = kernel.shape[0]
+    # m is the number of images,  h is the height of  an image, w the width
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+
+    # shape of the output
+    conv_h = h - (kw - 1)
+    conv_w = w - (kw - 1)
+
+    padded = np.zeros((m, h + kw, w + kw))
+    star_index = (kw - 1) / 2
+    padded[:, star_index:star_index + h, star_index:star_index + w] = images
+
+    # creating array for the output
+    conv_image = np.zeros((m, h, w))
+
+    for i in range(conv_h):
+        for j in range(conv_w):
+            crop = padded[:, i: i + kw, j:j + kw]
+            # beware to not sum the m number of images
+            conv_image[:, i, j] = np.sum(crop[:] * kernel, axis=(1, 2))
+
+    return conv_image
