40d36b07c4c12e2cfb277611d26249d6842dd8af
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-29 11:14:38 +0200

    much better, odd corrected

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -11,31 +11,32 @@ def convolve_grayscale_same(images, kernel):
     performs a same convolution on grayscale images with padding
     """
 
-    kh = kernel[:,0].shape[0]
-    kw = kernel[0,:].shape[0]
-    # print("kernel", kh , kw)   
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+       
 
     # m is the number of images,  h is the height of  an image, w the width
     m = images.shape[0]
     h = images.shape[1]
     w = images.shape[2]
 
-    paddingh = (kh - 1)  // 2
-    paddingw = (kw - 1)  // 2
     
-
+    paddingh = (kh )  // 2
+    paddingw = (kw )  // 2
+    
+    # padded aims to add zeros on the border of images
     padded = np.zeros((m, h +  2* paddingh , w + 2 * paddingw))
     padded[:, paddingh:paddingh + h, paddingw:paddingw + w] = images
 
-    # print("padding", padding)
     # creating array for the output
     conv_image = np.zeros((m, h, w))
 
     for i in range(h):
         for j in range(w):
+
             crop = padded[:, i: i + kh, j:j + kw]
             # beware to not sum the m number of images
             conv_image[:, i, j] = np.sum(crop[:] * kernel, axis=(1, 2))
-    #print("conv image", conv_image)
+
     return conv_image
 
