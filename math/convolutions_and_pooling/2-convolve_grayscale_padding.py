a9b89fc81441920dc61e8103957717cc57ddcb57
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-30 15:44:05 +0200

    shape bug fixing

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -17,8 +17,8 @@ def convolve_grayscale_padding(images, kernel, padding):
     
     (ph, pw) = padding
     
-    padded = np.pad(images, ((0, 0), (ph, ph), (pw , pw )), mode='constant')
-    print ("shape padded",ph, pw)
+    padded = np.pad(images,((0,0),( ph- 1 ,ph - 1 ), ( pw - 1, pw - 1)), mode='constant')
+    # print ("shape padded",padded.shape)
 
 
     conv_image = np.zeros((m, h + 2 * ph - 2, w + 2 * pw - 2))
@@ -27,6 +27,7 @@ def convolve_grayscale_padding(images, kernel, padding):
         for j in range(w):
 
             crop = padded[:, i: i + kh, j:j + kw]
+            # print("crop shape",crop.shape)
 
             # beware to not sum the m number of images
             conv_image[:, i, j] = np.sum(crop[:] * kernel, axis=(1, 2))
