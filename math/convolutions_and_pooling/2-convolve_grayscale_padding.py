444e8214ad23160ef0770fcccbedf12981f30724
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-30 18:22:07 +0200

    fixing algorithm

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -20,7 +20,7 @@ def convolve_grayscale_padding(images, kernel, padding):
     padded = np.pad(images,((0,0 ),(ph , ph), (pw, pw)), mode='constant')
  
 
-    conv_image = np.zeros((m, h + h + 2 * ph - kh + 1, w + 2 * pw - kw + 1))
+    conv_image = np.zeros((m, h + 2 * ph - kh + 1, w + 2 * pw - kw + 1))
 
     for i in range( h + 2 * ph - kh + 1):
         for j in range(w + 2 * pw - kw + 1):
