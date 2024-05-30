58f3ac07006714beaedce2e6c31d7515e98be534
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-30 18:26:31 +0200

    fixing algorithm

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -17,12 +17,17 @@ def convolve_grayscale_padding(images, kernel, padding):
 
     (ph, pw) = padding
 
+    # padding is exactly pw and pw in each sides
     padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+
+    # see the vidéo of deeplearningIA for explication
+    # of the size of conv_images
     conv_image = np.zeros((m, h + 2 * ph - kh + 1, w + 2 * pw - kw + 1))
 
     for i in range(h + 2 * ph - kh + 1):
         for j in range(w + 2 * pw - kw + 1):
             crop = padded[:, i: i + kh, j:j + kw]
+
             # beware to not sum the m number of images
             conv_image[:, i, j] = np.sum(crop[:] * kernel, axis=(1, 2))
 
