1bef0c1934462763fff284ef42adf3e61261cf50
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-05-30 18:24:45 +0200

    fixing algorithm

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -10,27 +10,20 @@ def convolve_grayscale_padding(images, kernel, padding):
     """
     performs a convolution on grayscale images with custom padding:
     """
-    
+
     (kh, kw) = kernel.shape
     # m is the number of images,  h is the height of  an image, w the width
     (m, h, w) = images.shape
-    
-    (ph, pw) = padding
-    
-    padded = np.pad(images,((0,0 ),(ph , ph), (pw, pw)), mode='constant')
- 
 
+    (ph, pw) = padding
+
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
     conv_image = np.zeros((m, h + 2 * ph - kh + 1, w + 2 * pw - kw + 1))
 
-    for i in range( h + 2 * ph - kh + 1):
+    for i in range(h + 2 * ph - kh + 1):
         for j in range(w + 2 * pw - kw + 1):
-
             crop = padded[:, i: i + kh, j:j + kw]
-            # print("crop shape",crop.shape)
-
             # beware to not sum the m number of images
             conv_image[:, i, j] = np.sum(crop[:] * kernel, axis=(1, 2))
 
-
     return conv_image
-
