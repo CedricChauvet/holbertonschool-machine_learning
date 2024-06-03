60012837b0764c07ba64e2c5e2931a751fb46c4a
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-06-03 17:18:03 +0200

    first implentatoin of the code

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -59,9 +59,9 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
                 x = np.multiply(A_prev_pad[:,i:i+kh, j:j+kw,0:c_prev], W[: ,: ,0:c_prev,c])
                 # print("x before sum",x.shape)
-                x = np.sum(x, axis=(1,2,3)) + b[0,0,0,c]
+                x = np.sum(x, axis=(1,2,3)) 
                 # print("x after",x.shape)
-                x = relu(x)
+                x = relu(x+ b[0,0,0,c])
                 conv[:, i, j, c] = x
                     
     return conv
