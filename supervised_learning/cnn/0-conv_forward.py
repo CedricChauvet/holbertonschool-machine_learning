493bd990ea76e0553fbef9e2b4152b4640e79448
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-03 10:46:30 +0200

    setting up files

---
+++ supervised_learning/cnn/0-conv_forward.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+project CNN
+by Ced
+"""
+import tensorflow.keras as K
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    pass
+
