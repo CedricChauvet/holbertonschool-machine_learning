8452ec927232b403d70fc875ee5b20b705f5d653
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-04 12:25:31 +0200

    ok

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -9,12 +9,15 @@ import numpy as np
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     Computes a pooling layer,
-    
-    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of the previous layer
-    kernel_shape is a tuple of (kh, kw) containing the size of the kernel for the pooling
+
+    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    containing the output of the previous layer
+    kernel_shape is a tuple of (kh, kw) containing the size of the
+    kernel for the pooling
     stride is a tuple of (sh, sw) containing the strides for the pooling
-    mode is a string containing either max or avg, indicating whether to perform maximum or average pooling, respectively
-    
+    mode is a string containing either max or avg, indicating whether
+    to perform maximum or average pooling, respectively
+
     Returns: the output of the pooling layer
     """
     # A_prev is the input of the convolution layer
@@ -35,16 +38,17 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     out_h = (h_prev - kh) // sh + 1
     out_w = (w_prev - kw) // sw + 1
     out_c = A_prev.shape[3]
-    
+
     A_pool = np.zeros((m, out_h, out_w, out_c))
-    
+
     for i in range(out_h):
         for j in range(out_w):
             for c in range(out_c):
-                x = A_prev[:, sh* i : sh * i + kh , sw * j: sw * j + kw, c]  
+                x = A_prev[:, sh * i: sh * i + kh, sw * j: sw * j + kw, c]
                 if mode == "max":
-                    x = np.max(x, axis=(1,2))
+                    x = np.max(x, axis=(1, 2))
                 if mode == "avg":
-                    x = np.mean(x, axis=(1,2))
+                    x = np.mean(x, axis=(1, 2))
                 A_pool[:, i, j, c] = x
-    return A_pool    
+
+    return A_pool
