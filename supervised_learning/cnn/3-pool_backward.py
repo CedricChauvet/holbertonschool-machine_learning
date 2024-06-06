372821b9e610bcdc78979b5b3688e471efa945e4
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-06-06 11:45:17 +0200

    exemple taken on https://www.linkedin.com/pulse/implementation-from-scratch-forward-back-propagation-layer-coy-ulloa/

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -11,33 +11,36 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     to document
     """
     (m, h_new, w_new, c_new) = dA.shape
-    (m, h_prev , w_prev, c) = A_prev.shape
+    (m, h_prev, w_prev, c) = A_prev.shape
     (kh, kw) = kernel_shape
     (sh, sw) = stride
+
     """
     print("dA shape", dA.shape)
     print("A_prev shape", A_prev.shape)
     print("kernel ", kernel_shape)
     print("stride ", stride)
     """
+
     dA_prev = np.zeros_like(A_prev)
 
     for i in range(m):
         for h in range(h_new):
             for w in range(w_new):
                 for f in range(c_new):
-                    
+
                     if mode == "max":
                         tmp = A_prev[i, h*sh:kh+(h*sh),
-                                        w*sw:kw+(w*sw), f]
+                                     w*sw:kw+(w*sw), f]
                         mask = (tmp == np.max(tmp))
                         dA_prev[i,
-                            h*(sh):(h*(sh))+kh,
-                            w*(sw):(w*(sw))+kw,
-                            f] += dA[i, h, w, f] * mask
-                    
+                                h*(sh):(h*(sh))+kh,
+                                w*(sw):(w*(sw))+kw,
+                                f] += dA[i, h, w, f] * mask
+
                     if mode == "avg":
-                        dA_prev[i, h*(sh):(h*(sh))+kh, w*(sw):(w*(sw))+kw, f] += (dA[i, h, w, f])/kh/kw
-    
+                        dA_prev[i, h * (sh): (h * (sh)) +
+                                kh, w * (sw): (w * (sw)) + kw, f]\
+                                      += (dA[i, h, w, f]) / (kh * kw)
+
     return dA_prev
-    
