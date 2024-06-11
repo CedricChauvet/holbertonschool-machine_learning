b7dd5b8a20eb83a9ca5d28fbdfa266de406995f7
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-11 16:02:54 +0200

    do an inception block

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -8,10 +8,9 @@ from tensorflow import keras as K
 
 def inception_block(A_prev, filters):
     """
-    to be defined
+    do a simple inception network
     """
     (F1, F3R, F3, F5R, F5, FPP) = filters
-    print("dim layers", F1, F3R, F3, F5R, F5, FPP)
 
     conv2d = K.layers.Conv2D(
         filters=F1,
@@ -20,8 +19,8 @@ def inception_block(A_prev, filters):
         activation=K.layers.ReLU(),
         kernel_initializer=K.initializers.he_normal(seed=0),
         )(A_prev)
-    
-    conv2d_1  = K.layers.Conv2D(
+
+    conv2d_1 = K.layers.Conv2D(
         filters=F3R,
         kernel_size=(1, 1),
         padding='same',
@@ -29,7 +28,6 @@ def inception_block(A_prev, filters):
         kernel_initializer=K.initializers.he_normal(seed=0),
         )(A_prev)
 
-    
     conv2d_2 = K.layers.Conv2D(
         filters=F3,
         kernel_size=(3, 3),
@@ -37,7 +35,7 @@ def inception_block(A_prev, filters):
         activation=K.layers.ReLU(),
         kernel_initializer=K.initializers.he_normal(seed=0)
         )(conv2d_1)
-    
+
     conv2d_3 = K.layers.Conv2D(
         filters=F5R,
         kernel_size=(1, 1),
@@ -53,24 +51,19 @@ def inception_block(A_prev, filters):
         activation=K.layers.ReLU(),
         kernel_initializer=K.initializers.he_normal(seed=0)
         )(conv2d_3)
-    
-    
-    
+
     max_pooling2d = K.layers.MaxPooling2D(
         strides=1, padding="same"
         )(A_prev)
 
-    
-    conv2d_5  = K.layers.Conv2D(
+    conv2d_5 = K.layers.Conv2D(
         filters=FPP,
         kernel_size=(1, 1),
         padding='same',
         activation=K.layers.ReLU(),
         kernel_initializer=K.initializers.he_normal(seed=0),
         )(max_pooling2d)
-    
-    OUT = K.layers.Concatenate()([ conv2d, conv2d_2, conv2d_4, conv2d_5])
 
+    OUT = K.layers.Concatenate()([conv2d, conv2d_2, conv2d_4, conv2d_5])
 
-    # model = K.Model(inputs=A_prev, outputs=conv2d_5)
     return OUT
