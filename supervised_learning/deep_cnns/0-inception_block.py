4fb19376f8a0d0d2e52a5921d236144a85b6f48b
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-11 15:59:32 +0200

    do an inception block

---
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Deep Convolutional Architectures project
+by Ced
+"""
+from tensorflow import keras as K
+
+
+def inception_block(A_prev, filters):
+    """
+    to be defined
+    """
+    (F1, F3R, F3, F5R, F5, FPP) = filters
+    print("dim layers", F1, F3R, F3, F5R, F5, FPP)
+
+    conv2d = K.layers.Conv2D(
+        filters=F1,
+        kernel_size=(1, 1),
+        padding='same',
+        activation=K.layers.ReLU(),
+        kernel_initializer=K.initializers.he_normal(seed=0),
+        )(A_prev)
+    
+    conv2d_1  = K.layers.Conv2D(
+        filters=F3R,
+        kernel_size=(1, 1),
+        padding='same',
+        activation=K.layers.ReLU(),
+        kernel_initializer=K.initializers.he_normal(seed=0),
+        )(A_prev)
+
+    
+    conv2d_2 = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        padding='same',
+        activation=K.layers.ReLU(),
+        kernel_initializer=K.initializers.he_normal(seed=0)
+        )(conv2d_1)
+    
+    conv2d_3 = K.layers.Conv2D(
+        filters=F5R,
+        kernel_size=(1, 1),
+        padding='same',
+        activation=K.layers.ReLU(),
+        kernel_initializer=K.initializers.he_normal(seed=0),
+        )(A_prev)
+
+    conv2d_4 = K.layers.Conv2D(
+        filters=F5,
+        kernel_size=(5, 5),
+        padding='same',
+        activation=K.layers.ReLU(),
+        kernel_initializer=K.initializers.he_normal(seed=0)
+        )(conv2d_3)
+    
+    
+    
+    max_pooling2d = K.layers.MaxPooling2D(
+        strides=1, padding="same"
+        )(A_prev)
+
+    
+    conv2d_5  = K.layers.Conv2D(
+        filters=FPP,
+        kernel_size=(1, 1),
+        padding='same',
+        activation=K.layers.ReLU(),
+        kernel_initializer=K.initializers.he_normal(seed=0),
+        )(max_pooling2d)
+    
+    OUT = K.layers.Concatenate()([ conv2d, conv2d_2, conv2d_4, conv2d_5])
+
+
+    # model = K.Model(inputs=A_prev, outputs=conv2d_5)
+    return OUT
