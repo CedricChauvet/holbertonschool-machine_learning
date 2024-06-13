01afad621acf8f27117efe5eb8706e1f301a141f
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-13 19:12:47 +0200

    structure but wrong dimensions

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Deep Convolutional Architectures project
+by Ced
+"""
+from tensorflow import keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    input0 = X
+    
+    for i in range(layers):
+        BN0 = K.layers.BatchNormalization()(input0)
+        ReLU0 = K.layers.Activation(activation='relu')(BN0)
+        conv_0 = K.layers.Conv2D(
+            filters=nb_filters+growth_rate,
+            kernel_size=(1, 1),
+            strides=(1, 1),
+            kernel_initializer=K.initializers.he_normal(seed=0),
+            padding="same")(ReLU0) 
+        
+        BN1 = K.layers.BatchNormalization()(conv_0)
+        ReLU1 = K.layers.Activation(activation='relu')(BN1)
+        conv_1 = K.layers.Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            kernel_initializer=K.initializers.he_normal(seed=0),
+            padding="same")(ReLU1)
+        input0 = K.layers.Concatenate()([conv_1, input0])
+
+    return input0,32
