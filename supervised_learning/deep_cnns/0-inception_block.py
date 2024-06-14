96609e4c59e445daff3585f0317fc2f7cc9de705
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-14 11:05:00 +0200

    fixing relu activation

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -16,35 +16,35 @@ def inception_block(A_prev, filters):
         filters=F1,
         kernel_size=(1, 1),
         padding='same',
-        activation="ReLU"
+        activation="relu"
         )(A_prev)
 
     conv2d_1 = K.layers.Conv2D(
         filters=F3R,
         kernel_size=(1, 1),
         padding='same',
-        activation="ReLU"
+        activation="relu"
         )(A_prev)
 
     conv2d_2 = K.layers.Conv2D(
         filters=F3,
         kernel_size=(3, 3),
         padding='same',
-        activation="ReLU"
+        activation="relu"
         )(conv2d_1)
 
     conv2d_3 = K.layers.Conv2D(
         filters=F5R,
         kernel_size=(1, 1),
         padding='same',
-        activation="ReLU"
+        activation="relu"
         )(A_prev)
 
     conv2d_4 = K.layers.Conv2D(
         filters=F5,
         kernel_size=(5, 5),
         padding='same',
-        activation="ReLU"
+        activation="relu"
         )(conv2d_3)
 
     max_pooling2d = K.layers.MaxPooling2D(
@@ -55,7 +55,7 @@ def inception_block(A_prev, filters):
         filters=FPP,
         kernel_size=(1, 1),
         padding='same',
-        activation="ReLU"
+        activation="relu"
         )(max_pooling2d)
 
     OUT = K.layers.Concatenate()([conv2d, conv2d_2, conv2d_4, conv2d_5])
