d830bf93c5e49310585da6ca8f440ce2a2a41f0b
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-14 09:48:44 +0200

    building a dense block

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -8,12 +8,13 @@ from tensorflow import keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     input0 = X
-    
+
+        
     for i in range(layers):
         BN0 = K.layers.BatchNormalization()(input0)
         ReLU0 = K.layers.Activation(activation='relu')(BN0)
         conv_0 = K.layers.Conv2D(
-            filters=nb_filters+growth_rate,
+            filters = 4 * growth_rate,
             kernel_size=(1, 1),
             strides=(1, 1),
             kernel_initializer=K.initializers.he_normal(seed=0),
@@ -22,11 +23,11 @@ def dense_block(X, nb_filters, growth_rate, layers):
         BN1 = K.layers.BatchNormalization()(conv_0)
         ReLU1 = K.layers.Activation(activation='relu')(BN1)
         conv_1 = K.layers.Conv2D(
-            filters=32,
+            filters= growth_rate,
             kernel_size=(3, 3),
             strides=(1, 1),
             kernel_initializer=K.initializers.he_normal(seed=0),
             padding="same")(ReLU1)
         input0 = K.layers.Concatenate()([input0, conv_1,])
 
-    return input0,32
+    return input0, nb_filters + layers * growth_rate
