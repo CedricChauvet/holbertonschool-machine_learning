51d324aec15437d70e0959d4a60a5b304fe2bc79
CedricChauvet <chauvetc.ujf@gmail.com>
2024-06-14 09:51:45 +0200

    building a dense block

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -7,27 +7,34 @@ from tensorflow import keras as K
 
 
 def dense_block(X, nb_filters, growth_rate, layers):
+    """
+    builds a dense block
+    - X is the output from the previous layer
+    - nb_filters is an integer representing the number of filters in X
+    - growth_rate is the growth rate for the dense block
+    layers is the number of layers in the dense block
+    You should use the bottleneck layers used for DenseNet-B
+    """
     input0 = X
 
-        
     for i in range(layers):
         BN0 = K.layers.BatchNormalization()(input0)
         ReLU0 = K.layers.Activation(activation='relu')(BN0)
         conv_0 = K.layers.Conv2D(
-            filters = 4 * growth_rate,
+            filters=4 * growth_rate,
             kernel_size=(1, 1),
             strides=(1, 1),
             kernel_initializer=K.initializers.he_normal(seed=0),
-            padding="same")(ReLU0) 
-        
+            padding="same")(ReLU0)
+
         BN1 = K.layers.BatchNormalization()(conv_0)
         ReLU1 = K.layers.Activation(activation='relu')(BN1)
         conv_1 = K.layers.Conv2D(
-            filters= growth_rate,
+            filters=growth_rate,
             kernel_size=(3, 3),
             strides=(1, 1),
             kernel_initializer=K.initializers.he_normal(seed=0),
             padding="same")(ReLU1)
-        input0 = K.layers.Concatenate()([input0, conv_1,])
+        input0 = K.layers.Concatenate()([input0, conv_1])
 
     return input0, nb_filters + layers * growth_rate
