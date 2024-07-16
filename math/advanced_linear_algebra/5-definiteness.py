0af7f509239847e912bb7d8ed86c6446219c547b
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-16 13:54:20 +0200

    last task

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -7,23 +7,26 @@ import numpy as np
 
 
 def definiteness(matrix):
+    """
+    produce the definiteness of matrice
+    by calculating eigen values
+    """
 
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    if len(matrix.shape) != 2 :
+    if len(matrix.shape) != 2:
         return None
     if (matrix.shape[0] != matrix.shape[1]):
         return None
-        
-    eigenvalues, eigenvectors = np.linalg.eig(matrix)
-    #print("eigenvalues",eigenvalues)
-    if all(eigenvalues >= 0) and any(eigenvalues == 0) :
+
+    eigenvalues, _ = np.linalg.eig(matrix)
+    if all(eigenvalues >= 0) and any(eigenvalues == 0):
         return "Positive semi-definite"
-    if all(eigenvalues <= 0) and any(eigenvalues == 0) :
+    if all(eigenvalues <= 0) and any(eigenvalues == 0):
         return "Negative semi-definite"
     if all(eigenvalues > 0):
         return "Positive definite"
     if all(eigenvalues < 0):
         return "Negative definite"
-    if any(eigenvalues > 0 ) and any(eigenvalues < 0 ):
-        return "Indefinite" 
+    if any(eigenvalues > 0) and any(eigenvalues < 0):
+        return "Indefinite"
