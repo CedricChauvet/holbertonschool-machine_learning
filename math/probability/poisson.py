3158985300ab42a89d05242d7bd22804d495c024
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-17 15:48:58 +0200

    first task scd try

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -6,29 +6,29 @@ probability project
 
 class Poisson():
     """
-    
+    Task 0  constructor
     """
 
     def __init__(self, data=None, lambtha=1.):
-        self.lambtha = lambtha  # lambtha is the expected number of occurences in a given time frame
-        self.data = data          # data is a list of the data to be used to estimate the distribution
- 
-        if  type(self.data) == list :
-            
-            if len(data)>=2:
-                s=0
+        """
+        lambtha is the expected number of occurences in a given time frame
+        data is a list of the data to be used to estimate the distribution
+        """
+        self.lambtha = lambtha
+        self.data = data
+
+        if type(self.data) is list:
+
+            if len(data) >= 2:
+                s = 0
                 for i in data:
-                    s+=i
-                mean = s /len(data)
+                    s += i
+                mean = s / len(data)
                 self.lambtha = mean
-
-
-
-
             else:
                 raise ValueError("data must contain multiple values")
 
-        elif self.data == None:
+        elif self.data is None:
             pass
 
         else:
@@ -36,5 +36,3 @@ class Poisson():
 
         if self.lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
-        
-        
