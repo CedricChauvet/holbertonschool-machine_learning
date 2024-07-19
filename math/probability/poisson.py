ae29173f4457b33f8f999d8049d54468f784bf83
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-19 14:01:09 +0200

    try3

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -41,7 +41,7 @@ class Poisson():
         Fonctions de masse de probabilité
         """
         e = 2.7182818285
-        print("n",k )
+        print("n",k ,"lambtha", self.lambtha)
         return pow(self.lambtha, k) * pow(e, -self.lambtha) / factorielle(k)
 
 
