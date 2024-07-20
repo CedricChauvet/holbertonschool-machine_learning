3b710dcbaa15a79085d7021a6f8be9463836af98
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-20 20:18:01 +0200

    try 1

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -14,7 +14,7 @@ class Normal():
         if data is None:
             self.mean = mean
             self.stddev = stddev
-            if stddev < 0:
+            if stddev < 0 or not isinstance(stddev, float):
                 raise ValueError ("stddev must be a positive value")
         
         else:
