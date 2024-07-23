b27d3a9378ace5df18f27637409e75d1ca009ebf
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-23 11:12:38 +0200

    task4 try1

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -10,6 +10,34 @@ def posterior(x, n, P, Pr):
     get the posterior by Bayes Theorem
     """
 
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that \
+is greater than or equal to 0")
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if any(np.where(P > 1, True, False)) or any(np.where(P < 0, True, False)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if any(np.where(Pr > 1, True, False)) or\
+       any(np.where(Pr < 0, True, False)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+
+
     variable = likelihood(x, n, P) * Pr /marginal(x, n, P, Pr)
     return variable
 
