837a4c80eca8aa441362ac225876bb12ba479e3f
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-07-23 10:40:29 +0200

    task2 try1

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -9,7 +9,8 @@ def intersection(x, n, P, Pr):
     """
     x is the number of patients that develop severe side effects
     n  is the total number of patients observed
-    P is a 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects
+    P is a 1D numpy.ndarray containing the various hypothetical
+        probabilities of developing severe side effects
     Pr is a 1D numpy.ndarray containing the prior beliefs of P
     """
     if not isinstance(n, int) or n <= 0:
@@ -18,28 +19,28 @@ def intersection(x, n, P, Pr):
     if not isinstance(x, int) or x < 0:
         raise ValueError("x must be an integer that \
 is greater than or equal to 0")
-    
+
     if x > n:
         raise ValueError("x cannot be greater than n")
-    
+
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    
+
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    
+
     if any(np.where(P > 1, True, False)) or any(np.where(P < 0, True, False)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    if any(np.where(Pr > 1, True, False)) or any(np.where(Pr < 0, True, False)):
+    if any(np.where(Pr > 1, True, False)) or\
+       any(np.where(Pr < 0, True, False)):
         raise ValueError("All values in Pr must be in the range [0, 1]")
-    
-    if not np.isclose(sum(Pr),1):
+
+    if not np.isclose(sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
     like = likelihood(x, n, P)
     return like * Pr
-
 
 
 def likelihood(x, n, P):
