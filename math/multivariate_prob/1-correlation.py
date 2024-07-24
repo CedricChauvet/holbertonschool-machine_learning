e65f1bf0ead26c4cb20bd8b723ee787226e89d03
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-24 15:17:18 +0200

    task1 try3

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -11,13 +11,15 @@ def correlation(C):
     calcul the correlation matrice give the convolution
     """
 
-    d = C.shape[0]
 
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[1] != d or len(C.shape) != 2:
+    if C.shape[1] != C.shape[0]  or len(C.shape) != 2:
         raise ValueError(" C must be a 2D square matrix")
+
     
+    d = C.shape[0]
+
     corr_mat = np.zeros((d,d))
     for i in range(d):
         for j in range(d):
