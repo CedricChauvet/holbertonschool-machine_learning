29989bfcb52601567ce9e4d741e345df62a15f9a
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-26 14:50:20 +0200

    task1 try1

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python3
-"""
-Project Dimensionality reduction
-By Ced+
-"""
 import numpy as np
-
 
 def pca(X, ndim):
     """
-    Takes X a matrix,
-    return Tr( reduction score)
-    """
-    n = X.shape[0]  # number of data points
-    d = X.shape[1]  # number of dimensions
+    Perform PCA on the given data matrix X and reduce its dimensionality to ndim.
 
-    X = X - np.mean(X, axis=0)
+    Parameters:
+    X (numpy.ndarray): Data matrix with shape (n, d) where n is the number of data points and d is the number of dimensions
+    ndim (int): Number of dimensions to reduce to
+
+    Returns:
+    T (numpy.ndarray): The transformed data matrix with shape (n, ndim)
+    """
+    # Center the data by subtracting the mean of each feature
+    X_centered = X - np.mean(X, axis=0)
     
-    # decompose X into SVD
-    U, S, Vh = np.linalg.svd(X, full_matrices=True)
-    # reduction dimension r
-    Ur = U[:,0:ndim]
-    Sr = np.diag(S[0:ndim])    
+    # Perform Singular Value Decomposition
+    U, S, Vt = np.linalg.svd(X_centered)
+    
+    # Select the top 'ndim' components
+    Ur = U[:, :ndim]
+    Sr = np.diag(S[:ndim])
+    
+    # Transform the data to the new reduced dimension space
     T = Ur @ Sr
+    
     return T
