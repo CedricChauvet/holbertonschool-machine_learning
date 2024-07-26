371c1c2e7f7999a89b1111cda519759920fd1eac
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-26 14:55:49 +0200

    task1 try2

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -17,7 +17,7 @@ def pca(X, ndim):
     X = X - np.mean(X, axis=0)
     
     # decompose X into SVD
-    U, S, _ = np.linalg.svd(X, full_matrices=True)
+    U, S, _ = np.linalg.svd(X, full_matrices=False)
     # reduction dimension r
     
     if ndim >= d:
