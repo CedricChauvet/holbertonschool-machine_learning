821fc9e1adedd1497caa3ab281acfb44537e9f46
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-29 10:34:59 +0200

    task1 try1

---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,16 @@
+"""
+Project Clusters
+By Ced+
+"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    set the centroids
+    """
+    n, d = X.shape
+
+    init = np.random.uniform(low=np.min(X, axis=0),
+                             high=np.max(X, axis=0), size=(k, d))
+    return init
