b8abb074ddf5df330209c16461c97102f0d45312
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-29 10:44:08 +0200

    task1 try handling errors

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -10,6 +10,12 @@ def initialize(X, k):
     """
     set the centroids
     """
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise TypeError("X must be an array")
+    if not isinstance(k, int) or k <= 0:
+        raise ValueError(" K must be a positive int")
+
     n, d = X.shape
 
     init = np.random.uniform(low=np.min(X, axis=0),
