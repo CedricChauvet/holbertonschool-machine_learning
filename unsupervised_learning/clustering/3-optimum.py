9cbe9e9e5fd601e8de4e7969eff445372c8b8e94
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-30 11:23:30 +0200

    task3 try2

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -18,7 +18,7 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None
     n, d1 = X.shape
 
-    if kmax < kmin:
+    if isinstance(kmin, int) and isinstance(kmax, int) and kmax < kmin:
         return None
     
     if not isinstance(kmax, int) or kmax < 0:
