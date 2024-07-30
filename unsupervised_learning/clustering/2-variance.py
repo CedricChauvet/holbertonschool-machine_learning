00b9e5cafc480ce243259d289aaaaee8ddaa0ab1
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-30 10:18:41 +0200

    task2 try1

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Project Clusters
+By Ced+
+"""
+import numpy as np
+
+
+def variance(X, C):
+    """
+    calculate variance
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    C is a numpy.ndarray of shape (k, d) containing the
+    centroid means for each cluster
+    """
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    n, d = X.shape
+
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+        return None
+    k, _ = C.shape
+
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+    clss = np.argmin(distances, axis=1)
+
+    var = np.sum((X[:] - C[clss]) ** 2)
+
+    return var
