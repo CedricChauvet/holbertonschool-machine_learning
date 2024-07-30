109dfb264b52faffa3634dd201fc255b5860c18d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-30 16:34:04 +0200

    task4 try2

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -11,12 +11,18 @@ def initialize(X, k):
     """
     intialze my gaussian
     """
-    
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+
     n, d = X.shape
+
+    if not isinstance(k, int) or k <= 0:
+        return None
+
     m, _ = kmeans(X, k, iterations=1000)
-    
+
     pi = np.zeros(k)
-    pi[:] = np.round(1 / k , decimals=8)
+    pi[:] = np.round(1 / k, decimals=8)
     s = np.repeat([np.identity(d)], repeats=k, axis=0)
 
     return pi, m, s
