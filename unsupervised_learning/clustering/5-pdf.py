15c1b905bb413468ee37d8d12eabe9806b3a5799
CedricChauvet <chauvetc.ujf@gmail.com>
2024-07-31 10:31:50 +0200

    task5 try 2

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -4,15 +4,22 @@ Project Clusters
 By Ced+
 """
 import numpy as np
-from scipy.stats import multivariate_normal
+
 
 def pdf(X, m, S):
     """
     Probability density function
     """
-    #print("X shape", X.shape)
-    pi = np.pi
     n, d = X.shape
-    pdf = np.zeros(n)
-    component_pdf = multivariate_normal(mean=m, cov=S).pdf(X)
-    return component_pdf
+    pi = np.pi
+
+    # Calcul de la constante de normalisation
+    det = np.linalg.det(S)
+    norm_const = 1.0 / (np.power((2 * np.pi), d / 2) * np.sqrt(det))
+
+    # Calcul de l'exponentielle
+    x_mu = X - m
+    inv_cov = np.linalg.inv(S)
+    result = np.exp(-0.5 * np.sum(np.dot(x_mu, inv_cov) * x_mu, axis=1))
+
+    return norm_const * result
