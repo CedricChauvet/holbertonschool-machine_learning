18396e75b9d26b8f679d6bc7918dfc267ccbad43
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-02 16:45:04 +0200

    task7 try1

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -17,6 +17,8 @@ def maximization(X, g):
         return None, None, None
     if g.shape[0] < 1:
         return None, None, None
+    if g.shape[1] != X.shape[0]:
+        return None, None, None
     n, d = X.shape
     k = g.shape[0]
 
