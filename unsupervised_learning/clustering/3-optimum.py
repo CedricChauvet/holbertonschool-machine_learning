355a0d417421bda851e49c831ca918cbef0669b4
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-08-04 17:45:12 +0200

    project finished

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -15,25 +15,26 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
     n, d = X.shape
-    
+
     if not isinstance(kmin, int) or kmin < 1:
         return None, None
-    
-    
+    if not isinstance(kmax, int) or kmax < 1:
+        return None, None
+
     results = []
     d_var = []
 
     if kmax is None or kmax >= n:
         kmax = n
 
-    for k in range(kmin, kmax+ 1 ):
+    for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         var = np.sum((X[:] - C[clss]) ** 2)
 
         if k == kmin:
             var_min = var
         results.append((C, clss))
-        
+
         d_var.append(var_min - var)
 
     return results, d_var
