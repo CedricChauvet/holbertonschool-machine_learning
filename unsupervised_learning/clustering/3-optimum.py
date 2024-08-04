30f773b5e1f66cc05d9defed3a0f393ec6ee3c7d
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-08-04 16:47:07 +0200

    solving issues

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -4,9 +4,6 @@ Project Clusters
 By Ced+
 """
 import numpy as np
-kmeans = __import__('1-kmeans').kmeans
-variance = __import__('2-variance').variance
-
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
@@ -37,12 +34,12 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
 
 
     for k in range(kmin, kmax+ 1 ):
-# initialize random k-centroids
+        # initialize random k-centroids
         centroid = np.random.uniform(low=np.min(X, axis=0),
                                         high=np.max(X, axis=0), size=(k, d))
 
         for i in range(iterations):
-            # get the closer centroid for each X
+
             distances = np.linalg.norm(X[:, np.newaxis] - centroid, axis=2)
             clss = np.argmin(distances, axis=1)
 
