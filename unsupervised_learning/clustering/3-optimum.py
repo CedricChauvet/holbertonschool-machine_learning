28cef06ac46e1d2d22a65bd7892c8e7cc5622ddb
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-08-04 16:23:09 +0200

    solving issues

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -35,13 +35,35 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if kmax is None or kmax >= n:
         kmax = n
 
+
     for k in range(kmin, kmax+ 1 ):
-        C, clss = kmeans(X, k, iterations)
-        var = variance(X, C)
+# initialize random k-centroids
+        centroid = np.random.uniform(low=np.min(X, axis=0),
+                                        high=np.max(X, axis=0), size=(k, d))
+
+        for i in range(iterations):
+            # get the closer centroid for each X
+            distances = np.linalg.norm(X[:, np.newaxis] - centroid, axis=2)
+            clss = np.argmin(distances, axis=1)
+
+            new_centroid = np.copy(centroid)
+
+            for j in range(k):
+                # new centroid
+                if len(np.where(clss == j)[0]) == 0:
+                    centroid[j] = np.random.uniform(np.min(X, axis=0),
+                                                    np.max(X, axis=0), d)
+                
+                else:
+                    centroid[j] = np.mean(X[np.where(clss == j)], axis=0)
+            # if centroid don't change, break
+            if np.array_equal(new_centroid, centroid):
+                break
 
         if k == kmin:
             var_min = var
-        results.append((C, clss))
+        var = np.sum((X[:] - centroid[clss]) ** 2)
+        results.append((centroid, clss))
         
         d_var.append(var_min - var)
 
