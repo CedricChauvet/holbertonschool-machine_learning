f4e24892f10f690f81e4d17be6e4d3067bbc8313
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-08-05 15:58:44 +0200

    task1 try1

---
+++ unsupervised_learning/hmm/1-regular.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Project Hiden Markov Model
+By Ced+
+"""
+import numpy as np
+
+
+def regular(P):
+    """
+    get transition matrice times a lot
+    """
+
+    n, n = P.shape
+    t = 300
+    s = np.zeros(n)
+    s[0] = 1
+    limit = np.linalg.matrix_power(P, t)
+    if np.any(limit == 0):
+        return None
+    else:
+        return s @ limit
