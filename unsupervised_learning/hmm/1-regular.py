9449a471dece0e631bb3eec3dea4112088e48ccf
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-08-05 16:04:41 +0200

    task1 try1

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -13,8 +13,8 @@ def regular(P):
 
     n, n = P.shape
     t = 300
-    s = np.zeros((1, n))
-    s[0] = 1
+    s = np.zeros((1,n))
+    s[0,0] = 1
     limit = np.linalg.matrix_power(P, t)
 
     if np.any(limit == 0):
