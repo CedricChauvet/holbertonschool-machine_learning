0975e29ec15c1e5e4185de81442d4350e885ec54
CedricChauvet <chauvetc.ujf@hotmail.com>
2024-08-06 20:07:35 +0200

    task3 try2

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -7,16 +7,19 @@ import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
+    """
+    forward algorithm
+    """
     n = len(Observation)
     num_S = Transition.shape[0]
 
     a = np.empty([num_S, n], dtype='float')
     # Base case
-    print("EM", Initial[:,0].shape)
-    a[:,0] = np.multiply(Initial[:,0], Emission[:, Observation[0]])
+    a[:, 0] = np.multiply(Initial[:, 0], Emission[:, Observation[0]])
 
-        # Recursive case
+    # Recursive case
     for t in range(1, n):
-        a[:,t] = np.multiply(Emission[:,Observation[t]], np.dot(Transition.T, a[:,t-1]))
-        
-    return (np.sum(a[:,n-1]), a)
+        a[:, t] = np.multiply(Emission[:, Observation[t]],
+                              np.dot(Transition.T, a[:, t-1]))
+
+    return (np.sum(a[:, n - 1]), a)
