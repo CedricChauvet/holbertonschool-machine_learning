c347d0336ac17b531854eee0cf449a1d4485d84d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-08 10:10:44 +0200

    task 5 try1

---
+++ unsupervised_learning/hmm/5-backward.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Project Hiden Markov Model
+By Ced+
+"""
+import numpy as np
+
+
+def backward(Observation, Emission, Transition, Initial):
+    '''
+    Input: Initial distribution v; Transition probability P; Emission
+    probability E;
+    Observations from HMM: Obers (Integer list with outcomes).
+    Output: (backward probabilities for all the time steps, likelihood value
+    based on 'Obser')
+    '''
+    n = len(Observation)
+    num_S = Transition.shape[0]
+    b = np.empty([num_S, n], dtype='float')
+    # Base case
+    b[:, n-1] = 1
+    # Recursive case
+    for t in reversed(range(n-1)):
+        b[:, t] = np.dot(Transition, np.multiply(Emission[:, Observation[t+1]],
+                                                 b[:, t+1]))
+
+    return (np.dot(Initial[:, 0], np.multiply(Emission[:, Observation[0]],
+                                              b[:, 0])), b)
