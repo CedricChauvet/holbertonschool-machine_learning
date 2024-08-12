42ca054b79bee699ed3ee8ecf1b4e563d811eb28
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-12 16:34:46 +0200

    task0 try1

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -9,6 +9,9 @@ import numpy as np
 
 
 class GaussianProcess:
+    """
+    This class represents a noiseless Gaussian process
+    """
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
 
         self.X = X_init
@@ -17,14 +20,13 @@ class GaussianProcess:
         self.sigma_f = sigma_f
         self.K = self.kernel(self.X, self.X)
 
-
     def kernel(self, X1, X2):
         """
         Radial Basis Function Kernel
         reutrn
         """
+
         # covariance kernel matrix, Gaussian process
-        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1)\
+            + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp((-0.5 / self.l**2) * sqdist)
-    
-    
