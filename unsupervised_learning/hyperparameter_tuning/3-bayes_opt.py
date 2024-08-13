6bf05d652691e65c9eefeba84f8bb92544d4c30d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-13 14:37:54 +0200

    task3 try1

---
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Prject Hyperparameters tunings
+By Ced+
+
+"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization():
+    """
+    initilise the new class
+    """
+    def __init__(self, f, X_init, Y_init, bounds,
+                 ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+        self.f = f
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples)
+        self.xsi = xsi
+        self.minimize = minimize
