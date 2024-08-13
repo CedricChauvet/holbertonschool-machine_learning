5d9f2b9cba689f1ac2baf34fc01573d9e332ba50
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-13 15:08:27 +0200

    task3 try1

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -16,6 +16,6 @@ class BayesianOptimization():
                  ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).T
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
