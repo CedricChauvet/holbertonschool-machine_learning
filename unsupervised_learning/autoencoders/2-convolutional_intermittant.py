704c08ed6ec3d76fde1ad65316029303937ea319
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-21 18:14:59 +0200

    task2 correct

---
+++ unsupervised_learning/autoencoders/2-convolutional_intermittant.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Project auto encoders
+Bu Ced+
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    """
+    cet autoencodeur utilise des filtres convolutif
+    suive de maxpool pour le retrecissement et de upsampling
+    La difficulté est dans le padding pour respecter les dimensions
+    on se retrouve avec des dimensions impaires
+    """
+
+    # creation de l'encodeur, 1ere moitié
+    encoder = keras.Sequential()
+    encoder.add(keras.Input(shape=(input_dims)))
+    for n in filters:
+        encoder.add(keras.layers.Conv2D(n,
+                                        activation='relu',
+                                        kernel_size=(3, 3),
+                                        padding='same'))
+
+        encoder.add(keras.layers.MaxPooling2D((2, 2), padding='same'))
+
+    # création du decoder et de la recontruction de l'image
+    decoder = keras.Sequential()
+    for n in filters[::-1][0:2]:
+        decoder.add(keras.layers.Conv2D(n,
+                                        activation='relu',
+                                        padding='same',
+                                        kernel_size=(3, 3)))
+
+        decoder.add(keras.layers.UpSampling2D((2, 2)))
+
+    decoder.add(keras.layers.
+                Conv2D(16,
+                       activation='sigmoid',
+                       padding='valid',
+                       kernel_size=(3, 3)))
+
+    decoder.add(keras.layers.UpSampling2D((2, 2)))
+    decoder.add(keras.layers.
+                Conv2D(input_dims[-1],
+                       activation='sigmoid',
+                       padding='same',
+                       kernel_size=(3, 3)))
+
+    # utiliser add permet de stacker les layers, pratique
+    # mais attention au dimensions entre 2 sequentials
+    auto = keras.Sequential()
+    auto.add(encoder)
+    auto.add(decoder)
+
+    # Compile the autoencoder, binary crossentrepy pour les 10 labels
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    # auto.summary()
+    return encoder, decoder, auto
