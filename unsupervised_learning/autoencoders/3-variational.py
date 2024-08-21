b39830b0b5e9a6c82b10758e5248aba57d3a3585
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-21 19:43:28 +0200

    task3 try1

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -5,6 +5,7 @@ Bu Ced+
 """
 import tensorflow.keras as keras
 
+
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     ok see the vidz!!!
@@ -15,27 +16,21 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     encoder_input = keras.layers.Input(shape=(input_dims,))
     x = encoder_input  # variable modulable
 
-    # print("conv shape", keras.backend.int_shape(x))
-    
-    #x = keras.layers.Flatten()(x)
-    
-
     for units in hidden_layers:
         x = keras.layers.Dense(units, activation='relu')(x)
-    
+
     z_mu = keras.layers.Dense(latent_dims, name="mu")(x)
     z_sigma = keras.layers.Dense(latent_dims, name="sigma")(x)
 
-
-    
-    z = keras.layers.Lambda(sample_z, output_shape=(latent_dims,), name='z')([z_mu, z_sigma])
+    z = keras.layers.Lambda(sample_z,
+                            output_shape=(latent_dims,),
+                            name='z')([z_mu, z_sigma])
 
     # premier modele
-    encoder = keras.models.Model(inputs=encoder_input, outputs=[z_mu, z_sigma, z], name='encoder')
-    #encoder.summary()
+    encoder = keras.models.Model(inputs=encoder_input,
+                                 outputs=[z_mu, z_sigma, z],
+                                 name='encoder')
 
-
-    
     # Decoder
     # 2 eme input positionné derriere le botleneck
     decoder_input = keras.layers.Input(shape=(latent_dims,))
@@ -48,7 +43,6 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
     decoder = keras.models.Model(inputs=decoder_input, outputs=decoder_output)
 
-
     auto_input = keras.layers.Input(shape=input_dims)
     _, _, z = encoder(auto_input)
     decoded = decoder(z)
@@ -58,7 +52,12 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     return encoder, decoder, auto
 
+
 def sample_z(args):
-    z_mu,z_sigma = args
-    eps = keras.backend.random_normal(shape = (keras.backend.shape(z_mu)[0], keras.backend.int_shape(z_mu)[1]))
+    """
+    tips
+    """
+    z_mu, z_sigma = args
+    eps = keras.backend.random_normal(shape=(keras.backend.shape(z_mu)[0],
+                                             keras.backend.int_shape(z_mu)[1]))
     return z_mu + keras.backend.exp(z_sigma / 2) * eps
