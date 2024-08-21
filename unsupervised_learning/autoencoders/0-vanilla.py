f26b5ef8e2dbb748a76f9ea84a835ce71d8ebcff
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-21 14:30:10 +0200

    task0 try help

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -9,39 +9,44 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     input_dims is an integer containing the dimensions of the model input
-    
-    hidden_layers is a list containing the number of nodes for each hidden layer in the encoder, respectively
+    hidden_layers is a list containing the number of nodes for each hidden
+    layer in the encoder, respectively
     the hidden layers should be reversed for the decoder
-    latent_dims is an integer containing the dimensions of the latent space representation
+    latent_dims is an integer containing the dimensions of the latent
+    space representation
     """
 
-def autoencoder(input_dims, hidden_layers, latent_dims):
     # Encoder
+    # creation d'une input pour les images
     encoder_input = keras.layers.Input(shape=(input_dims,))
-    x = encoder_input
-    
+    x = encoder_input  # variable modulable
+
     for units in hidden_layers:
         x = keras.layers.Dense(units, activation='relu')(x)
-    
     latent = keras.layers.Dense(latent_dims, activation='relu')(x)
+
+    # premier modele
     encoder = keras.models.Model(inputs=encoder_input, outputs=latent)
-    
+
     # Decoder
+    # 2 eme input positionné derriere le botleneck
     decoder_input = keras.layers.Input(shape=(latent_dims,))
     x = decoder_input
-    
+
     for units in reversed(hidden_layers):
         x = keras.layers.Dense(units, activation='relu')(x)
-    
+
+    # sortie du decodeur
     decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
     decoder = keras.models.Model(inputs=decoder_input, outputs=decoder_output)
-    
+
     # Autoencoder (encoder + decoder)
-    auto_input = encoder_input
+    auto_input = encoder_input   # mise en cache
     auto_output = decoder(encoder(auto_input))
+
+    # dernier modele
     auto = keras.models.Model(inputs=auto_input, outputs=auto_output)
     # auto.summary()
     # Compile the autoencoder
     auto.compile(optimizer='adam', loss='binary_crossentropy')
-    # git test
     return encoder, decoder, auto
