e47d3602a6b9c45b31bf35325896295a39a1cdfc
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-01 13:07:31 +0200

    task1 try2

--- unsupervised_learning/gan/1-wgan_clip.py
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -1,53 +1,84 @@
+#!/usr/bin/env python3
+"""
+Project GAN
+By Ced+
+"""
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-class WGAN_clip(keras.Model) :
-    
-    def __init__( self, generator , discriminator , latent_generator, real_examples, batch_size=200, disc_iter=2, learning_rate=.005):
-        super().__init__()                         # run the __init__ of keras.Model first. 
+
+class WGAN_clip(keras.Model):
+    """
+    class with new loss function and clipping
+    IDK what do!
+    """
+
+    def __init__(self, generator, discriminator,
+                 latent_generator, real_examples,
+                 batch_size=200, disc_iter=2,
+                 learning_rate=.005):
+        super().__init__()
         self.latent_generator = latent_generator
-        self.real_examples    = real_examples
-        self.generator        = generator
-        self.discriminator    = discriminator
-        self.batch_size       = batch_size
-        self.disc_iter        = disc_iter
-        
-        self.learning_rate    = learning_rate
-        self.beta_1=.5                               # standard value, but can be changed if necessary
-        self.beta_2=.9                               # standard value, but can be changed if necessary
-        
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
+
+        self.learning_rate = learning_rate
+        self.beta_1 = .5
+        self.beta_2 = .9
+
         # define the generator loss and optimizer:
-        self.generator.loss = lambda x : - tf.math.reduce_mean( input_tensor = x, axis=None, keepdims=False, name=None)
+        self.generator.loss = lambda x: \
+            - tf.math.reduce_mean(input_tensor=x)
 
-        self.generator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.generator.compile(optimizer=generator.optimizer , loss=generator.loss )
-        
+        self.generator.optimizer = keras.optimizers.\
+            Adam(learning_rate=self.learning_rate,
+                 beta_1=self.beta_1,
+                 beta_2=self.beta_2)
+        self.generator.compile(optimizer=generator.optimizer,
+                               loss=generator.loss)
+
         # define the discriminator loss and optimizer:
-        self.discriminator.loss = lambda x,y : - tf.math.reduce_mean( input_tensor = x, axis=None, keepdims=False, name=None)\
-            + tf.math.reduce_mean( input_tensor = y, axis=None, keepdims=False, name=None)
+        self.discriminator.loss =\
+            lambda x, y: - tf.math.reduce_mean(input_tensor=x)\
+            + tf.math.reduce_mean(input_tensor=y)
 
-        self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer , loss=discriminator.loss )
-       
-    
+        self.discriminator.optimizer =\
+            keras.optimizers.Adam(learning_rate=self.
+                                  learning_rate,
+                                  beta_1=self.beta_1,
+                                  beta_2=self.beta_2)
+        self.discriminator.compile(optimizer=discriminator.optimizer,
+                                   loss=discriminator.loss)
+
     # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
-        if not size :
-            size= self.batch_size
+        """
+        get fake sample
+        """
+        if not size:
+            size = self.batch_size
         return self.generator(self.latent_generator(size), training=training)
 
     # generator of fake samples of size batch_size
     def get_real_sample(self, size=None):
-        if not size :
-            size= self.batch_size
+        """
+        get real sample
+        """
+        if not size:
+            size = self.batch_size
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
-        random_indices  = tf.random.shuffle(sorted_indices)[:size]
+        random_indices = tf.random.shuffle(sorted_indices)[:size]
         return tf.gather(self.real_examples, random_indices)
-        
-             
+
     def train_step(self, useless_argument):
+        """
+        use to  train gen and discriminator
+        """
         # 1. Entraînement du discriminateur
         for _ in range(self.disc_iter):
 
@@ -73,13 +104,10 @@ class WGAN_clip(keras.Model) :
             self.discriminator.optimizer.\
                 apply_gradients(zip(discr_grads,
                                     self.discriminator.trainable_variables))
-            
-            
+
             for w in self.discriminator.trainable_weights:
                 w.assign(tf.clip_by_value(w, -1, 1))
-            #self.discriminator.kernel.trainable = True
-            #self.discriminator.bias.trainable = False
-            
+
         # 2. Entraînement du générateur
         with tf.GradientTape() as tape:
             # get a fake sample
