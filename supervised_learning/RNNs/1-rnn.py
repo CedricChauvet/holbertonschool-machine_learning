a1891072ef952a49e68348070f1507a75404bd95
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-03 14:42:05 +0200

    trying

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -4,6 +4,8 @@ Rnns project
 use the rnn class of the rnn module task0
 """
 import numpy as np
+
+
 def rnn(rnn_cell, X, h_0):
     """
     forward prop for a simple RNN
@@ -13,19 +15,19 @@ def rnn(rnn_cell, X, h_0):
 
     t, m, i = X.shape
     _, h = h_0.shape
-    
+
     # shape of H
     H = np.zeros((t+1, m, h))
     H[0] = h_0
-    
+
     # Initialize Y with the correct shape
     Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))  # Assuming rnn_cell.Wy exists
 
     # make n time steps for the rnn
     for j in range(t):
         H[j+1], Y[j] = rnn_cell.forward(H[j], X[j])
-    
-    print("Y shape:", Y.shape)
-    print("Y[0] shape:", Y[0].shape)
-    
+
+    # print("Y shape:", Y.shape)
+    # print("Y[0] shape:", Y[0].shape)
+
     return H, Y
