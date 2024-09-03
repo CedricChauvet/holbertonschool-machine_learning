27e3860d5fac7439b4fd5bcfa8c86f25a48a78f8
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-03 16:50:04 +0200

    hard try!

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -7,45 +7,49 @@ import numpy as np
 
 
 class GRUCell():
-    def __init__(self,i, h, o):
-        self.i = i
-        self.h = h
-        self.o = o
+    """
+    hint, watch
+    https://penseeartificielle.fr/comprendre-lstm-gru-fonctionnement-schema/
+    """
 
-        self.Wz= np.random.normal(size=(h+i, h))
-        self.bz = np.zeros((1,h))
-        self.Wr= np.random.normal(size=(h+i, h))
-        self.br = np.zeros((1,h))
-        self.Wh= np.random.normal(size=(h+i, h))
-        self.bh = np.zeros((1,h))
-        self.Wy= np.random.normal(size=(h,o ))
-        self.by = np.zeros((1,o))
-        
+    def __init__(self, i, h, o):
+        self.Wz = np.random.normal(size=(h+i, h))
+        self.bz = np.zeros((1, h))
+        self.Wr = np.random.normal(size=(h+i, h))
+        self.br = np.zeros((1, h))
+        self.Wh = np.random.normal(size=(h+i, h))
+        self.bh = np.zeros((1, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.by = np.zeros((1, o))
+
     def forward(self, h_prev, x_t):
         """
-        toute la diffuculté est d'ecrire efficacement les equations de la cellule GRU
+        toute la diffuculté est d'ecrire efficacement les
+        equations de la cellule GRU
         """
+
         z = np.concatenate((h_prev, x_t), axis=1)
         out_Reset = self.sigmoid(np.dot(z, self.Wr) + self.br) * h_prev
         out_Update = self.sigmoid(np.dot(z, self.Wz) + self.bz)
-        out_h= np.tanh(np.dot(np.concatenate((out_Reset, x_t), axis=1),self.Wh) + self.bh)
-        
+        out_h = np.tanh(np.dot(np.concatenate((out_Reset, x_t), axis=1),
+                               self.Wh) + self.bh)
+
         # print("out_Reset", out_Reset.shape)
         # print("out_Update", out_Update.shape)
         # print("out_h", out_h.shape)
-
-        # h_i0 et h_next
+        # h_i0 est entre h-prev et h_next
         h_i0 = h_prev * (1 - out_Update)
         h_next = h_i0 + (out_Update * out_h)
         y = np.dot(h_next, self.Wy) + self.by
         y = self.softmax(y)
+        return h_next, y
 
-        # print("out_Reset", out_Reset.shape)
-
-        return h_next, y 
-    
-    def sigmoid(self,x):
+    def sigmoid(self, x):
+        """
+        compute sigmoid values for each sets of scores in x
+        """
         return 1 / (1 + np.exp(-x))
+
     def softmax(self, x):
         """Compute softmax values for each sets of scores in x."""
         e_x = np.exp(x)
