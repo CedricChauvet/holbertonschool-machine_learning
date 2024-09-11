290d971d5a1b2e4b56cb90b9b32cbc09f3e7569c
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-11 09:54:09 +0200

    wednesday work

--- supervised_learning/time_series/preprocess_data.py
+++ supervised_learning/time_series/preprocess_data.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/env python3
 """
 code to make time series data in a dataset, variable are resized
@@ -35,3 +36,28 @@ df.to_csv('output_btc1.csv', index=True)
 
 # print(df_init.head())
 # print(df.head())
+=======
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Lecture du fichier CSV avec parse_dates
+df = pd.read_csv('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv')
+
+
+# Transformer le timestamp en date avec heure
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
+df.set_index('Timestamp', inplace=True)
+# Échantillonnage des données (une ligne sur 15)
+df = df.iloc[-1440::15]
+
+# Sélection et renommage des colonnes d'intérêt
+df = df[[ 'Open', 'Volume_(BTC)', 'Weighted_Price']]
+df.rename(columns={'Volume_(BTC)': 'Volume'}, inplace=True)
+
+# Affichage des 10 premières lignes
+print(df.head(50))
+
+# Sauvegarde en CSV
+df.to_csv('output.csv')
+
+>>>>>>> 18ee7c2 (wednesday work)
