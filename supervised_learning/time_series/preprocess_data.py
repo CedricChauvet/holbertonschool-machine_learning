6aa6373446c5c6ddf0b5957d22ff5b7d338676fa
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-15 11:20:49 +0200

    preprocessing_valid

--- supervised_learning/time_series/preprocess_data.py
+++ supervised_learning/time_series/preprocess_data.py
@@ -2,52 +2,30 @@
 """
 code to make time series data in a dataset, variable are resized
 """
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
-import tensorflow as tf
-
+import numpy as np
 df = pd.read_csv('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv')
 
-df["Timestamp"] = pd.to_datetime(df["Timestamp"], unit='s').dt.strftime('%Y/%m/%d - %H:%M')
-# give date as index
+print("Number of corrupted Nan values", np.isnan(df).sum())
+df = df.dropna()  # Supprimer les valeurs manquantes
+
+df["Timestamp"] = pd.to_datetime(df["Timestamp"], unit='s')
+# Deuxième ligne : Formater le datetime en chaîne de caractères
+df["Timestamp"] = df["Timestamp"].dt.strftime('%Y/%m/%d - %H:%M')
+
+# 1. give date as index
 df.set_index("Timestamp", inplace=True)
-print(df.dtypes)
 
-# prendre la fin du document avec un intervalle de 5 minutes
+# 2. Appliquez la conversion float32 uniquement à ces colonnes
+list_columns = ["Open", "High", "Low", "Close", "Volume_(BTC)",
+                "Volume_(Currency)", "Weighted_Price"]
+df[list_columns] = df[list_columns].astype('float32')
+print(df.dtypes)  # print the type of data inside df
+
+# 3. prendre la fin du document avec un intervalle de 60 minutes
 df = df[-100000::60]
-print ("taille de la donnée", len(df))
-df.to_csv('output_btc0.csv', index=False)
 
+print("taille de la donnée", len(df))
+print("interval des données du", df.index[0], "au", df.index[-1])
 
-# print("x shape", X.shape)
-# print("y shape", y.shape) 
-# Diviser en ensembles d'entraînement et de test
-# train_size = int(len(X) * 0.8)
-# X_train, X_test = X[:train_size], X[train_size:]
-# y_train, y_test = y[:train_size], y[train_size:]
-
-# np.savez("./out_btc.npz", X, y)
-
-
-
-
-# # Créer des tf.data.Dataset
-# train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-# test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
-
-
-
-# # Créer des tf.data.Dataset
-# train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-# test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
-
-# # Préparer les datasets pour l'entraînement et le test
-# # batch_size = 32
-# # train_dataset = train_dataset.shuffle(10000).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-# # test_dataset = test_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
-
-# # Sauvegarder les datasets
-# tf.data.Dataset.save(train_dataset, "train_dataset")
-# tf.data.Dataset.save(test_dataset, "test_dataset")
+df.to_csv('output_btc0.csv', index=True)
