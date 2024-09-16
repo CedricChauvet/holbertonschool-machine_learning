720172bc30d4a536b492575654d31432d90bd0d3
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-16 17:31:01 +0200

    pycode

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -12,45 +12,42 @@ def bag_of_words(sentences, vocab=None):
     creates a bag of words embedding matrix:
     """
 
-    if vocab is None:
+    if vocab is None:  # si le vocabulaire n'est pas donné
         features = set()
-
-
         for sentence in sentences:
             words = sentence.split(" ")
             for word in words:
                 word = word.lower()
                 word = formatter(word)
-                features.add(word)  
+                features.add(word)
+
+        features = sorted(features)
     else:
-        features = vocab    
-    features = sorted(features)            
+        features = vocab
+
     embeddings = np.zeros((len(sentences), len(features)), dtype=int)
 
     for i, sentence in enumerate(sentences):
         sentence = sentence.lower()
         for j, word in enumerate(features):
             c = compter(word, sentence)
-            embeddings[i, j] = c    
-
-        
-        
+            embeddings[i, j] = c
+    # embedding est une matrice qui compte les mots du  vocabulaire
     return embeddings, np.array(features)
+
 
 def compter(word, phrase):
     """
     Compte les occurrences d'un mot spécifique dans une phrase.
-    
     Args:
     word (str): Le mot à rechercher.
     phrase (str): La phrase dans laquelle chercher.
-    
+
     Returns:
     int: Le nombre d'occurrences du mot dans la phrase.
     """
     # Crée un pattern qui recherche le mot entier
     pattern = r'\b' + re.escape(word) + r'\b'
-    
     # Trouve toutes les occurrences et retourne leur nombre
     occurrences = len(re.findall(pattern, phrase, re.IGNORECASE))
     return occurrences
@@ -58,18 +55,12 @@ def compter(word, phrase):
 
 def formatter(word):
     """
-    Compte les occurrences d'un mot spécifique dans une phrase.
-    
+    permet au vocabulaire d'ezviter les doublons
     Args:
     word (str): Le mot à rechercher.
-    phrase (str): La phrase dans laquelle chercher.
-    
-    Returns:
-    int: Le nombre d'occurrences du mot dans la phrase.
+    Returns: le mot formatté.
     """
-    # Crée un pattern qui recherche le mot entier
     resultat = re.sub(r"(\w+)'s\b", r"\1", word)
     resultat = re.sub(r"[!,?.]", "", resultat)
 
     return resultat
-
