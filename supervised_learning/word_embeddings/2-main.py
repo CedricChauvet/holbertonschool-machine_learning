4ad64ff8e03a4ae98ecb6debf5d1a12f96d08efc
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-18 11:37:10 +0200

    task 2 try2

--- supervised_learning/word_embeddings/2-main.py
+++ supervised_learning/word_embeddings/2-main.py
@@ -6,5 +6,5 @@ from gensim.test.utils import common_texts
 word2vec_model = __import__('2-word2vec').word2vec_model
 
 print(common_texts[:2])
-# w2v = word2vec_model(common_texts, min_count=1)
-# print(w2v.wv["computer"])
+w2v = word2vec_model(common_texts, min_count=1)
+print(w2v.wv["computer"])
--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,8 +3,7 @@
 NLP project
 by Ced
 """
-import re
-import numpy as np
+
 from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
 
@@ -14,16 +13,16 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
     build and train, word2vec model
     """
     model = Word2Vec()
-    model.vector_size = 100
-    model.min_count = 1
-    model.window = 5
-    model.negative = 5
+    model.vector_size = vector_size
+    model.min_count = min_count
+    model.window = window
+    model.negative = negative
     model.sg =  0 if cbow else 1
-    model.epoch = 5
-    model.seed = 0
-    model.workers = 1
-    model.build_vocab(common_texts)
-    model.corpus_count = len(common_texts)
-    model.train(common_texts, total_examples=model.corpus_count, epochs=model.epoch)
+    model.epochs = epochs
+    model.seed = seed
+    model.workers = workers
+    model.build_vocab(sentences)
+    model.corpus_count = len(sentences)
+    model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
 
     return model
