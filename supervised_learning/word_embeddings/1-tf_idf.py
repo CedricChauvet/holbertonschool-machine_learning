06b9f9b5ac6b0474dc02b56aeff97a37fb51fdff
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-18 15:03:18 +0200

    fixing

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,16 +5,18 @@ by Ced
 """
 import re
 import numpy as np
+import sklearn as sk
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 def tf_idf(sentences, vocab=None):
     """
     Convert a collection of raw documents to a matrix of TF-IDF features
-    """    
+    """
+
     X = np.zeros((len(sentences), len(vocab)))
-    vectorizer =  TfidfVectorizer(vocabulary=vocab)
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
     Y = vectorizer.fit_transform(sentences).todense()
 
     feat = vectorizer.get_feature_names_out()
     return Y, feat
-
