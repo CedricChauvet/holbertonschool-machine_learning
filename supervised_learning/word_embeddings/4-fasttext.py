84f7ba09c62e2ce0850aa8a208ab47fdb5fad189
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-18 13:16:30 +0200

    Task4 same as Task2

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+NLP project,
+google search engine
+by Ced
+"""
+import gensim
+
+
+def fasttext_model(sent, vector_size=100, min_count=5,
+                   negative=5, window=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
+
+    cbow = not cbow
+    model = gensim.models.FastText(
+        sentences=sent,
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        workers=workers,
+        sg=cbow,
+        negative=negative,
+        seed=seed
+    )
+
+    # common_texts = gensim.test.utils.common_texts
+    # Build the vocabulary
+    model.build_vocab(sent)
+
+    # Train the model for many epochs
+    model.train(sent, total_examples=model.corpus_count, epochs=epochs)
+    return model
