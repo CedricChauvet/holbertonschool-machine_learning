4149c5fe13df3c37cdb07e50aa7559e6be211fcd
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-18 11:34:31 +0200

    task2 try1

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -11,9 +11,19 @@ from gensim.models import Word2Vec
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
-    train and text word2vec model
+    build and train, word2vec model
     """
+    model = Word2Vec()
+    model.vector_size = 100
+    model.min_count = 1
+    model.window = 5
+    model.negative = 5
+    model.sg =  0 if cbow else 1
+    model.epoch = 5
+    model.seed = 0
+    model.workers = 1
+    model.build_vocab(common_texts)
+    model.corpus_count = len(common_texts)
+    model.train(common_texts, total_examples=model.corpus_count, epochs=model.epoch)
 
-    model = Word2Vec(common_texts, vector_size=vector_size, window=window, min_count=min_count, workers=workers,  seed=seed, negative=negative, sg=cbow)
-    model.train(common_texts, total_examples=len(common_texts), epochs=epochs)
     return model
