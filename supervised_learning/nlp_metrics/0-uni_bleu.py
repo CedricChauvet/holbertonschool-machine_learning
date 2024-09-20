4afe6cf5970558aa79a7bd04604bddfe1625fadc
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-20 14:11:40 +0200

    pycode

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -5,6 +5,7 @@ calculates the unigram BLEU score for a sentence
 """
 import numpy as np
 
+
 def uni_bleu(references, sentence):
     """
     param references: list of reference translations
@@ -14,31 +15,29 @@ def uni_bleu(references, sentence):
 
     count = 0
     for word in sentence:
-        refs=[]
-        # print('word', word, sentence.count(word)) 
+        refs = []
+        # print('word', word, sentence.count(word))
         for ref in references:
-        #     print('ref', ref)   
-              refs.append(ref.count(word))    
+            # print('ref', ref)
+            refs.append(ref.count(word))
         # print('refs', refs,"max", max(refs), "\n")
 
         count += min(sentence.count(word), max(refs))
 
-    P1 = count/len(sentence)  
-    # print('P1', P1) 
-    
+    P1 = count/len(sentence)
+    # print('P1', P1)
+
     r = find_closest(references, sentence)
     ref_closest = len(references[r])
 
     if len(sentence) < ref_closest:
-        BP = np.exp(1 - ( ref_closest / len(sentence)))
-    else: 
+        BP = np.exp(1 - (ref_closest / len(sentence)))
+    else:
         BP = 1
 
-    # print('len(sentence)', len(sentence), 'len(references)', len(references[0]))
-
-    #print('len(sentence)', len(sentence), 'len(references)', len(references[:]))
     # print('BP', BP)
     return P1 * BP
+
 
 def find_closest(references, sentence):
     """
@@ -49,5 +48,5 @@ def find_closest(references, sentence):
     ref_len = []
     for ref in references:
         ref_len.append(abs(len(ref) - len(sentence)))
-    
+
     return ref_len.index(min(ref_len))
