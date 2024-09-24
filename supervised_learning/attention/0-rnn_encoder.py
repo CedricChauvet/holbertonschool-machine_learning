6987708ad7e94f54abb8bb8983e0e2fea6acc124
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-24 09:49:29 +0200

    task0 try1

---
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Regularization project
+by Ced
+"""
+import numpy as np
+import tensorflow as tf
+
+class RNNEncoder:
+    """RNN encoder class"""
+    def __init__(self, vocab, embedding, units, batch):
+        """Constructor"""
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.units = units
+        self.gru = tf.keras.layers.GRU(units, recurrent_initializer='glorot_uniform')e
+        self.batch = batch
+
+    def initialize_hidden_state(self):
+        """Initializes the hidden states for the RNN cell to a tensor of zeros"""
+        return np.zeros((self.batch, self.units))
+
+    def __call__(self, x, initial):
+        """Forward prop"""
+        embeddings = self.embedding[x]
+        outputs, hidden = self.gru.forward(initial, embeddings)
+        return outputs, hidden
