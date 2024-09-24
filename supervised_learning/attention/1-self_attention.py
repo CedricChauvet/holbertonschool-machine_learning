d759d5f30ea19b1f09d42cf67862790632b9a9a4
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-24 11:40:44 +0200

    task2 try1

---
+++ supervised_learning/attention/1-self_attention.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Regularization project
+by Ced
+"""
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """
+    self attention class
+    inherits from a keras layer
+    """
+
+    def __init__(self, units):
+        """Constructor"""
+        super(SelfAttention, self).__init__()
+        self.W = tf.keras.layers.Dense(units)
+        self.U = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1) # sure?
+
+    def call(self, s_prev, hidden_states):
+        s_prev = tf.expand_dims(s_prev, 1)  
+        decoder = self.W(s_prev)
+        # print("decoder",decoder.shape)
+        encoder = self.U(hidden_states)
+        # print("encoder",encoder.shape)
+        score = self.V(tf.nn.tanh(encoder + decoder))
+       # Calcul des poids d'attention
+        weights = tf.nn.softmax(score, axis=1)
+        
+        # Calcul du vecteur de contexte
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
+        
+        return context, weights
