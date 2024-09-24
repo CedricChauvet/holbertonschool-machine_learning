ea1f00014939fdca83dcfd1f08c28c605c79d762
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-24 10:17:43 +0200

    isokay

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -5,21 +5,29 @@ by Ced
 """
 import tensorflow as tf
 
-class RNNEncoder:
-    """RNN encoder class"""
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """RNN encoder class
+       inherits from a keras layer
+    """
     def __init__(self, vocab, embedding, units, batch):
         """Constructor"""
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
         self.units = units
-        self.gru = tf.keras.layers.GRU(units, recurrent_initializer='glorot_uniform')
+        self.gru = tf.keras.layers.GRU(units,
+                                       recurrent_initializer='glorot_uniform',
+                                       return_sequences=True,
+                                       return_state=True)
         self.batch = batch
 
     def initialize_hidden_state(self):
-        """Initializes the hidden states for the RNN cell to a tensor of zeros"""
+        """
+        Initializes the hidden states for the RNN cell to
+        a tensor of zeros"""
         return tf.zeros((self.batch, self.units))
 
     def __call__(self, x, initial):
         """Forward prop"""
-        #embeddings = self.embedding[x]
-        outputs, hidden = self.gru.forward(initial, embeddings)
-        return None, None
+        embeddings = self.embedding(x)
+        outputs, hidden = self.gru(embeddings, initial)
+        return outputs, hidden
