9646df509328771c17a3ec629f3ea9bb352f7187
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-25 11:39:03 +0200

    merge

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -4,7 +4,7 @@ Attention project
 by Ced
 """
 import tensorflow as tf
-
+SelfAttention = __import__('1-self_attention').SelfAttention
 
 class RNNDecoder(tf.keras.layers.Layer):
     """
@@ -12,6 +12,7 @@ class RNNDecoder(tf.keras.layers.Layer):
     """
 
     def __init__(self, vocab, embedding, units, batch):
+        super(RNNDecoder, self).__init__()
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
         self.gru = tf.keras.layers.GRU(units,
                                 recurrent_initializer='glorot_uniform',
@@ -27,5 +28,12 @@ class RNNDecoder(tf.keras.layers.Layer):
         and s is a tensor of shape (batch, units) containing the new
         decoder hidden state
         """
+        c = SelfAttention(s_prev.shape[1])(s_prev, hidden_states)[0]
+        print("c shape", c.shape)
+        print("X shape", x.shape)
+        concat1 = tf.concat((c, x), axis=1)
 
+
+        #print("concat", concat.shape)
+        y,s = self.gru(concat1, initial_state=s_prev)
         return y, s
