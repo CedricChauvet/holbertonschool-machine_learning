1a1ccc1f7d37a4fd41447270c49f2f2debcf1bcb
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-25 12:22:45 +0200

    pycode task2

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -6,6 +6,7 @@ by Ced
 import tensorflow as tf
 SelfAttention = __import__('1-self_attention').SelfAttention
 
+
 class RNNDecoder(tf.keras.layers.Layer):
     """
     build a decoder class for RNN
@@ -15,33 +16,32 @@ class RNNDecoder(tf.keras.layers.Layer):
         super(RNNDecoder, self).__init__()
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
         self.gru = tf.keras.layers.GRU(units,
-                                recurrent_initializer='glorot_uniform',
-                                return_sequences=True,
-                                return_state=True)
+                                       recurrent_initializer='glorot_uniform',
+                                       return_sequences=True,
+                                       return_state=True)
         self.F = tf.keras.layers.Dense(vocab)
 
     def call(self, x, s_prev, hidden_states):
         """
-        calling method 
+        calling method
         returns y the tensor of shape (batch, vocab) containing
         the output word as a one hot vector in the target vocabulary
         and s is a tensor of shape (batch, units) containing the new
         decoder hidden state
         """
-        c , _= SelfAttention(s_prev.shape[1])(s_prev, hidden_states)
+        c, _ = SelfAttention(s_prev.shape[1])(s_prev, hidden_states)
         x = self.embedding(x)
-   
-   
+
         # Concatenate attention context vector and embedded input
         x = tf.concat([tf.expand_dims(c, 1), x], axis=-1)
-        
+
         # Pass through GRU
         output, state = self.gru(x, initial_state=s_prev)
-        
+
         # Reshape output
         output = tf.reshape(output, (-1, output.shape[2]))
-        
+
         # Pass through final dense layer
         y = self.F(output)
-        
+
         return y, state
