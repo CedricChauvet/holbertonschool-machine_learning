abb31f9ff5da813b8195bb5c6a896be54d24998d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-25 16:42:23 +0200

    task3 try0

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -1,5 +1,6 @@
 """
-
+Attention project
+By Ced
 """
 import tensorflow as tf
 import numpy as np
@@ -13,4 +14,10 @@ def positional_encoding(max_seq_len, dm):
     Returns: a numpy.ndarray of shape (max_seq_len, dm) containing
     the positional encoding vectors
     """
-    pass
+    PE = np.zeros((max_seq_len, dm))
+    for i in range(max_seq_len):
+        for j in range(0, dm, 2):
+            PE[i, j] = np.sin(i / (10000 ** ((2 * j) / dm)))
+            PE[i, j + 1] = np.cos(i / (10000 ** ((2 * j) / dm)))
+
+    return PE
