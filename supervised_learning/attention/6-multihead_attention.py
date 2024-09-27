43c816b9a03dda38890b749892ebea7450f3df14
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-27 13:12:10 +0200

    task 5 try2

--- supervised_learning/attention/6-multihead_attention.py
+++ supervised_learning/attention/6-multihead_attention.py
@@ -4,6 +4,9 @@ Attention project
 By Ced
 """
 import tensorflow as tf
+from tensorflow import math, matmul, reshape, shape, transpose, cast, float32
+from keras.backend import softmax
+ 
 sdp_attention = __import__('5-sdp_attention').sdp_attention
 
 
@@ -13,6 +16,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
     """
     
     def __init__(self, dm, h):
+        super(MultiHeadAttention, self).__init__()
         self.h = h
         self.dm = dm
         self.depth = dm // h
@@ -21,10 +25,30 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         self.Wv = tf.keras.layers.Dense(dm)
         self.linear = tf.keras.layers.Dense(dm)
     
-    def __call__(self, Q, K, V, mask= None):
-        """
-        Call method
-        """
-        # Q shape (batch seq_len_q, dk)
-        print("q", Q.shape)
-        sdp_attention(self.Wq * Q, self.Wk * K,self.Wv * V, mask)
+
+    def reshape_tensor(self, x, heads, flag):
+        if flag:
+            # Tensor shape after reshaping and transposing: (batch_size, heads, seq_length, -1)
+            x = reshape(x, shape=(shape(x)[0], shape(x)[1], self.h, -1))
+            x = transpose(x, perm=(0, 2, 1, 3))
+        else:
+            # Reverting the reshaping and transposing operations: (batch_size, seq_length, d_k)
+            x = transpose(x, perm=(0, 2, 1, 3))
+            x = reshape(x, shape=(shape(x)[0], shape(x)[1], self.dm))
+        return x
+ 
+    def call(self, Q, K, V, mask=None):
+        # Rearrange the queries to be able to compute all heads in parallel
+        q_reshaped = self.reshape_tensor(self.Wq(Q), self.h, True)
+        k_reshaped = self.reshape_tensor(self.Wk(K), self.h, True)
+        v_reshaped = self.reshape_tensor(self.Wv(V), self.h, True)
+        o_reshaped, attention_weights = sdp_attention(q_reshaped, k_reshaped, v_reshaped, mask)
+ 
+ 
+        # Rearrange back the output into concatenated form
+        output = self.reshape_tensor(o_reshaped, self.h, False)
+        # Resulting tensor shape: (batch_size, input_seq_length, d_v)
+ 
+        # Apply one final linear projection to the output to generate the multi-head attention
+        # Resulting tensor shape: (batch_size, input_seq_length, d_model)
+        return self.linear(output), attention_weights
