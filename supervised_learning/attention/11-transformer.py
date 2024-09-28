d9919a0ba4e180b86631c5df88b3cdf592ecd759
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-28 15:45:03 +0200

    fin du projet

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -9,6 +9,9 @@ Decoder = __import__('10-transformer_decoder').Decoder
 
 
 class Transformer(tf.keras.Model):
+    """
+    whole transformer model
+    """
     def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
                  max_seq_input, max_seq_target, drop_rate=0.1):
         super(Transformer, self).__init__()
@@ -22,7 +25,7 @@ class Transformer(tf.keras.Model):
              encoder_mask, look_ahead_mask, decoder_mask):
         """
         build the transformer model as described in the paper
-        on calling the model, it should return the output of the transformer
+        on calling, it should return the model of the transformer
         """
         encoder_output = self.encoder(inputs, training, encoder_mask)
         decoder_output = self.decoder(target, encoder_output,
