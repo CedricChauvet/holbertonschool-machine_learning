d746540aa3d54e1338e28a6a45b55c53a6391acd
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-30 15:05:53 +0200

    task0 data_train issue

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -4,15 +4,13 @@ Transformer Applications project
 By Ced
 """
 import tensorflow_datasets as tfds
-import transformers
+import transformers as tf
 
-class Dataset():
+
+class Dataset:
     def __init__(self):
-        """
-        Class constructor
-        """
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
+        self.data_train = tf.data.Dataset.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
+        self.data_valid = tf.data.Dataset.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
         
     def tokenize_dataset(self, data):
@@ -22,9 +20,6 @@ class Dataset():
 
         tokenizer_pt = transformers.AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
         tokenizer_en = transformers.AutoTokenizer.from_pretrained('bert-base-uncased')
-        # for pt, en in self.data_train.take(1):
-        #     print("Portuguese: ", pt.numpy().decode('utf-8'))
-        #     print("English:   ", en.numpy().decode('utf-8'))
     
         return tokenizer_pt, tokenizer_en
 
@@ -34,12 +29,3 @@ class Dataset():
         """
         return None
     
-    
-
-
-    def display_text(slef, pt, en):
-        pt_text = pt.numpy().decode('utf-8')
-        en_text = en.numpy().decode('utf-8')
-        print("Portugais:", pt_text)
-        print("Anglais  :", en_text)
-        print()
