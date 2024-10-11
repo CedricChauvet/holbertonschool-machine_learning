0f36c65a242b5447e98df594afba67be651e422c
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-11 15:15:05 +0200

    pycode fix

--- supervised_learning/transformer_apps/1-dataset.py
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -62,7 +62,11 @@ class Dataset():
         nouveau_sep_id = 8193
 
         # Exemple de tokenization manuelle avec vos propres IDs
-        pt_tokens = [nouveau_cls_id] + self.tokenizer_pt.encode(pt.numpy().decode('utf-8'), add_special_tokens=False) + [nouveau_sep_id]
-        en_tokens = [nouveau_cls_id] + self.tokenizer_en.encode(en.numpy().decode('utf-8'), add_special_tokens=False) + [nouveau_sep_id]
-        #print("encode en", self.tokenizer_en.encode(en))
+        pt_tokens = ([nouveau_cls_id] +
+                     self.tokenizer_pt.encode(pt.numpy().
+                     decode('utf-8'), add_special_tokens=False)
+                     + [nouveau_sep_id])
+        en_tokens = ([nouveau_cls_id] + self.tokenizer_en.encode(en.numpy().
+                     decode('utf-8'), add_special_tokens=False)
+                     + [nouveau_sep_id])
         return pt_tokens, en_tokens
