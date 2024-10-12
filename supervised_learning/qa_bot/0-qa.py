80fa1e441446188e41b851445cd7f476328f873d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-12 17:46:30 +0200

    pycodestyle

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -13,16 +13,17 @@ from transformers import BertTokenizer
 def question_answer(question, reference):
     """
     question is a string containing the question to answer
-    reference is a string containing the reference document from which to find the answer
+    reference is a string containing the reference document
+        from which to find the answer
     Returns: a string containing the answer
     """
-    # max length of the input 
+    # max length of the input
     max_len = 512
     # Load the tokenizer from BertTokenizer
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     # Load the model from tensorflow hub
     model = hub.load("https://tfhub.dev/see--/bert-uncased-tf2-qa/1")
-    
+
     # encode the inputs, question and reference
     encoded = tokenizer.encode_plus(
         question,
@@ -31,10 +32,9 @@ def question_answer(question, reference):
         max_length=max_len,
         truncation=True,
         padding='max_length',
-        return_tensors='tf'# tensorflow in output
+        return_tensors='tf'   # tensorflow in output
     )
 
-        
     # Extrayez input_ids et attention_mask
     input_ids = encoded['input_ids']
     attention_mask = encoded['attention_mask']
@@ -51,25 +51,25 @@ def question_answer(question, reference):
         attention_mask,
         token_type_ids
     ]
-
     result = model(inputs)
 
-    
-    # Inspecter le résultat
-    print("Type de result:", type(result))
-    print("\nNombre d'éléments dans result:", len(result))
-    
-    print("\nPour le premier élément (start_scores):")
-    print("Type:", type(result[0]))
-    print("Shape:", result[0].shape)
-    
-    print("\nPour le deuxième élément (end_scores):")
-    print("Type:", type(result[1]))
-    print("Shape:", result[1].shape)
+    # # Inspecter le résultat
+    # print("Type de result:", type(result))
+    # print("\nNombre d'éléments dans result:", len(result))
+
+    # print("\nPour le premier élément (start_scores):")
+    # print("Type:", type(result[0]))
+    # print("Shape:", result[0].shape)
+
+    # print("\nPour le deuxième élément (end_scores):")
+    # print("Type:", type(result[1]))
+    # print("Shape:", result[1].shape)
 
     # Find the tokens with the highest start and end scores
+
     start_index = int(np.argmax(result[0][0][1:]))
     end_index = int(np.argmax(result[1][0][1:]))
-    answer_tokens = input_ids[0][start_index+1 : end_index+2]
+    answer_tokens = input_ids[0][start_index+1: end_index+2]
     answer = tokenizer.decode(answer_tokens)
+
     return answer
