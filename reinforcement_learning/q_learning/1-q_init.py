1f3c39d0080b20e22ee5e007ce7fbb9e2d280b68
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-21 10:15:17 +0200

    task1 pycode

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -6,13 +6,11 @@ by Ced
 import numpy as np
 
 
-
 def q_init(env):
     """
-    Initializes the Q-table
+    Initializes the Q-table with np.zeros
     4 actions in the Q-table up-down-left-right
     16 states in the Q-table lors de table 4*4
     """
     Q = np.zeros((env.observation_space.n, env.action_space.n))
     return Q
-    
