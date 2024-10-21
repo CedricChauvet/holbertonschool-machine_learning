45d1102065bcdb722b232999270bb69f2a8c1c88
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-21 10:53:09 +0200

    task3 creating files

---
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Project Q learning
+by Ced
+"""
+import numpy as np
+
+def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+    pass
