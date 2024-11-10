50252fbb0ea71bc8fa4b170b1f1105c26a7d6488
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-10 16:25:09 +0100

    try4

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -5,20 +5,21 @@ By Ced
 """
 import numpy as np
 
+
 def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
                   gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
     """
     run 5000 episodes of TD(λ) algorithm
     """
-    current_epsilon = epsilon
-
     for episode in range(episodes):
+        # reset the environment and sample one episode
+        # le jouer debute en haut a gauche
         state = env.reset()[0]
         
         n_states, n_actions = Q.shape
-        action = get_action(state, Q, current_epsilon)
+        action =  get_action(state, Q, epsilon)
 
-        # Initialize eligibility traces
+        # Initialize Q-table and eligibility traces
         E = np.zeros((n_states, n_actions))
 
         done = False
@@ -28,7 +29,8 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
             next_state, reward, done, truncated, _ = env.step(action)
             
             # Choose next action according to epsilon-greedy policy
-            next_action = get_action(next_state, Q, current_epsilon)
+
+            next_action = get_action(next_state, Q, epsilon)
 
             # Calculate TD error
             td_error = (reward + gamma * Q[next_state, next_action] - Q[state, action])
@@ -47,8 +49,8 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
             action = next_action
             
         # Decay epsilon après chaque épisode
-        # current_epsilon = max(min_epsilon, current_epsilon * (1 - epsilon_decay))
-        current_epsilon = max(min_epsilon, current_epsilon * (1 - epsilon_decay))
+        epsilon = max(min_epsilon, epsilon * (1 - epsilon_decay))
+
     return Q
 
 def get_action(state, Q, epsilon):
