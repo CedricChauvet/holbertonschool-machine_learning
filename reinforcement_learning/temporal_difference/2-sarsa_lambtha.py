af13f68dfd80133e9b96e235aea5250cfb64bde0
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-10 17:02:05 +0100

    try4

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -39,13 +39,13 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
             actual = Q[state, action]
             delta = target - actual
             
-            E *= gamma * lambtha
-           
-            
-
-            Q += alpha * delta * E
-            
             E[state, action] += 1
+            
+            Q += alpha * delta * E            
+            
+            E *= gamma * lambtha
+            
+            
             state, action = next_state, next_action
 
             
