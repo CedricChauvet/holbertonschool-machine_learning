6dfcfc6a8c3154fd093c8a5113a479dedd60fb94
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-11 16:42:37 +0100

    task1

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -16,3 +16,30 @@ def policy(matrix, weight):
     z = np.dot(matrix, weight)
     exp = np.exp(z)
     return exp / np.sum(exp)
+
+
+def policy_gradient(state, weight):
+    """
+    find the gradient J of the policy
+    """
+    # takes useful variables
+    n_states, n_actions = weight.shape
+
+    # Initialiser le gradient avec la bonne forme
+    gradient = np.zeros((n_states, n_actions))
+
+    policy_value = policy(state, weight)
+    # stochastic policy based on previous task
+    if np.random.random() > policy_value[0]:
+        action = 1
+    else:
+        action = 0
+
+    # Calculer le gradient pour chaque action
+    for a in range(n_actions):
+        if a == action:
+            gradient[:, a] = state * (1 - policy_value[a])
+        else:
+            gradient[:, a] = -state * policy_value[a]
+
+    return action, gradient
