095e4c5bec09652441734edda7bc91fe31eddcaf
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-13 15:16:18 +0100

    pycode

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -13,10 +13,9 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
     """
     scores = []
     n_states, n_actions = env.observation_space.shape[0], env.action_space.n
-    
+
     weights = np.random.rand(n_states, n_actions)
     # grad = np.zeros(weights.shape)
-
 
     for i in range(nb_episodes):
         state, _ = env.reset()
@@ -26,21 +25,22 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         gradients = []
         while not done:
             action, grad = policy_gradient(state, weights)
-            # print("action", action)
-            
+            # print("action", action)*
+
             next_state, reward, done, truncated, _ = env.step(action)
 
             gradients.append(grad)
             rewards.append(reward)
-            
+
             state = next_state
-            weights += alpha * sum([grad * (gamma ** t) * reward for t, (grad, reward) in enumerate(zip(gradients, rewards))])
+            weights += alpha * sum([grad * (gamma ** t) * reward for t,
+                                   (grad, reward) in enumerate(
+                                    zip(gradients, rewards))])
 
             if truncated:
                 break
-        # weights += alpha * sum([grad * (gamma ** t)   for t, (grad) in enumerate(grads)])
-        
+
         scores.append(sum(rewards))
-        
+
         print("EP: " + str(i) + " Score: " + str(sum(rewards)))
     return scores
