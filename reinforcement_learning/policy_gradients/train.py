643f9c8812f8ec392f2c029f8d11273c3f19a34a
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-15 10:40:04 +0100

    final task completed

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -4,43 +4,63 @@ This project is about policy gradient
 By Ced
 """
 import numpy as np
+import gymnasium as gym
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     """
-    θ(t+1) = θ(t) + α ∑(γ^t ∇θ log π(at|st; θ) Rt)
+    Task 3, training the cartpole agent
+    displays the score every episode
+    render human every 1000 episodes
     """
+
+    # scores array
     scores = []
+
+    # get the number of states and actions from the environment gymnasium
     n_states, n_actions = env.observation_space.shape[0], env.action_space.n
 
     weights = np.random.rand(n_states, n_actions)
-    # grad = np.zeros(weights.shape)
 
     for i in range(nb_episodes):
-        state, _ = env.reset()
+
+        # set done to False a initial state
         done = False
-        # print("state: ", state)
+
+        # display an episode every 1000 episodes
+        # if show_result is set to True
+        if show_result and i % 1000 == 0:
+            env = gym.make('CartPole-v1', render_mode="human")
+        else:
+            env = gym.make('CartPole-v1', render_mode=None)
+
+        state, _ = env.reset()
         rewards = []
         gradients = []
+
         while not done:
+            # get the action and gradient
             action, grad = policy_gradient(state, weights)
-            # print("action", action)*
+            state, reward, done, truncated, _ = env.step(action)
 
-            next_state, reward, done, truncated, _ = env.step(action)
-
+            # apply the policy gradient
             gradients.append(grad)
             rewards.append(reward)
 
-            state = next_state
             weights += alpha * sum([grad * (gamma ** t) * reward for t,
                                    (grad, reward) in enumerate(
                                     zip(gradients, rewards))])
 
+            # quit after 500 steps
             if truncated:
                 break
 
+        # close each episode, close the window
+        env.close()
         scores.append(sum(rewards))
 
+        # display the score every episode
         print("EP: " + str(i) + " Score: " + str(sum(rewards)))
+
     return scores
