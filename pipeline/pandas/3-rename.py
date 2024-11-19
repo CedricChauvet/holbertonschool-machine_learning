d5d3dd6e0399e8b64f5efd880ffd0012a6dec888
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-19 12:13:43 +0100

    task2 pycode

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,6 +7,10 @@ import pandas as pd
 
 
 def rename(df):
+    """
+    doing some renaming, and converting the timestamp to datetime
+    selecting only the columns Datetime and Close
+    """
     df = df.rename(columns={'Timestamp': 'Datetime'})
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
     df = df[['Datetime', 'Close']]
