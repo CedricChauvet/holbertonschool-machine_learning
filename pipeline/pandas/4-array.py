1e54542ff7da3f8c40a2f1cec881ee18d7181d19
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-19 12:18:57 +0100

    task 4 pycode

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Pandas project
+By Ced
+"""
+import pandas as pd
+
+
+def array(df):
+    """
+    selecting the last 10 rows of the 2 columns High and Close
+    and convert them into an array numpy
+    """
+    df = df[['High', 'Close']]
+    df = df.tail(10)
+    df_array = df.to_numpy()
+    return df_array
