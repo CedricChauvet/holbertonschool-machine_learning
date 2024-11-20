672370e7d4c049fa5748e6dbaeabfe7ff21d5b66
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-20 11:02:06 +0100

    task12 try0

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Pandas project
+By Ced
+"""
+import pandas as pd
+index = __import__('10-index').index
+concat = __import__('11-concat').concat
+
+
+def hierarchy(df1, df2):
+    """
+    create a hierarchy for the data
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2.loc[(df2.index <= 1417417980) & (df2.index >= 1417411980)]
+    df1 = df1.loc[(df1.index <= 1417417980) & (df1.index >= 1417411980)]
+    df = pd.concat([df2, df1],  keys=['bitstamp', 'coinbase'])
+    df = df.sort_index(level='Timestamp')
+    df = df.swaplevel(0, 1)
+    return df
