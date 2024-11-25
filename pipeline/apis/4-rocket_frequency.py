78b4c9147e1f0be3488a06a3e237144fa1552c0c
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-25 14:20:06 +0100

    pycode

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -10,14 +10,19 @@ def main():
     """
     main function
     """
+
+    # get the data from the API launch AND rockets
     r = requests.get('https://api.spacexdata.com/v4/launches/')
     r2 = requests.get('https://api.spacexdata.com/v4/rockets/')
-    launches = r.json() 
+    launches = r.json()
     rockets = r2.json()
+
+    # initialize count
     falcon_1_count = 0
     falcon_9_count = 0
     falcon_heavy_count = 0
 
+    # before we store the ids corresponding to the rockets
     for i in rockets:
         if i.get('name') == "Falcon 1":
             falcon_1_id = i.get('id')
@@ -26,22 +31,22 @@ def main():
         if i.get('name') == "Falcon Heavy":
             falcon_heavy_id = i.get('id')
 
-
-    #print("rockets", rockets[0])
+    # we loop through the launches to count the number of
+    # launches for each rocket
     for i in range(len(launches)):
-        #print("rocket name", rockets[i].get('name'))
-       
+
         if launches[i].get('rocket') == falcon_1_id:
             falcon_1_count += 1
         if launches[i].get('rocket') == falcon_9_id:
             falcon_9_count += 1
         if launches[i].get('rocket') == falcon_heavy_id:
             falcon_heavy_count += 1
-    
+
     print(f"Falcon 9: {falcon_9_count}")
     print(f"Falcon 1: {falcon_1_count}")
     print(f"Falcon Heavy: {falcon_heavy_count}")
-    
+
+
 if __name__ == "__main__":
 
     main()
