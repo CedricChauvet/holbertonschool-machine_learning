4a1fcefc0386a533d5e0664d04ec7ec11129adad
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-25 15:32:51 +0100

    try2

--- pipeline/databases/1-first_table.sql
+++ pipeline/databases/1-first_table.sql
@@ -1,6 +1,6 @@
 -- Commentaire clair
 -- Créer la base de données si elle n'existe pas
-CREATE TABLE IF NOT EXISTS fisrt_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
