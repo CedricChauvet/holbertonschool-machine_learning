6bf4a3d337a60732be81729058f345ae60567988
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-25 11:29:40 +0100

    creating files

---
+++ pipeline/apis/3-first_launch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+APIs project
+By Ced
+"""
+import requests
+
+def main():
+    """
+    main function
+    """
+    print("hello")
+
+if __name__ == "__main__":
+
+    main()
