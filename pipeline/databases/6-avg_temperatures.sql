eeac0f12c07d3548725186eb7cd710a42e39ddde
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-26 18:11:31 +0100

    task6 try2

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -2,7 +2,8 @@
 -- mysql -u root -p hbtn_0c_0 < temperatures.sql
 -- CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
-SELECT city, AVG(value)
+
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY AVG(value) DESC;
+ORDER BY avg_temp DESC;
