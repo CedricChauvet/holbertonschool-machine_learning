affe4ca46c3220c1c3678213f7a1b0054ea44e90
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-27 10:43:40 +0100

    task10 try2

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 
 SELECT t1.name AS genre, COUNT(t2.show_id) AS number_of_shows
 FROM tv_genres t1
-LEFT JOIN tv_show_genres t2 ON t1.id = t2.genre_id
+INNER JOIN tv_show_genres t2 ON t1.id = t2.genre_id
 GROUP BY t1.name
 ORDER BY number_of_shows DESC;
