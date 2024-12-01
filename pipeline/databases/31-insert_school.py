f7c4bfe4734c0fad77ee6e51dcfb0b67ffe0f97e
CedricChauvet <chauvetc.ujf@gmail.com>
2024-12-01 16:24:02 +0100

    try2

--- pipeline/databases/31-insert_school.py
+++ pipeline/databases/31-insert_school.py
@@ -8,5 +8,5 @@ import pprint
 
 def insert_school(mongo_collection, name, address):
     """ inserts a new document in a collection based on kwargs """
-    mongo_collection.insert_one({"name": name, "address": address})
+    mongo_collection.insert({"name": name, "address": address})
     
