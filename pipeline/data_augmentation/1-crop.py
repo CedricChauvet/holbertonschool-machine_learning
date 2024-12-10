e8bad6cb041652d0f8d08b17b02180abfdec31fd
CedricChauvet <chauvetc.ujf@gmail.com>
2024-12-10 10:15:40 +0100

    task1 try0

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -8,10 +8,9 @@ import tensorflow as tf
 
 def crop_image(image, size):
     """
-    
+    still using tf.image, this time to crop the image    
     """
-    height, width, _ = size
-    print("hg", height, width)
-    cropped = tf.keras.layers.RandomCrop( 200, 200)(image)  # crop image
-    
-    print("cropped", cropped)
+    height, width, channel = size
+
+    cropped = tf.image.random_crop(image, size=[height, width, channel])
+    return cropped
