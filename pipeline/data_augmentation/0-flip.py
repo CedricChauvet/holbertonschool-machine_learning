0733895d9e4009fb92c85d8f9ff7fef4a8870c0f
CedricChauvet <chauvetc.ujf@gmail.com>
2024-12-10 09:59:07 +0100

    task0 try0

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+pipeline data augmentation
+Task 0 Flip
+"""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """
+    * image is a 3D tf.Tensor containing the image to flip
+    * Returns the flipped image
+    """
+    return tf.image.flip_left_right(image)  # flip image horizontally    
