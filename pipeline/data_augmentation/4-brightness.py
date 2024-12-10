2950268e1bb188735b25a814ad9b2371dfd6165e
CedricChauvet <chauvetc.ujf@gmail.com>
2024-12-10 10:42:12 +0100

    task4 try1

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -11,4 +11,5 @@ def change_brightness(image, max_delta):
     change the brightness of an image with max_delta
     max_delta is the maximum amount the image should be brightened
     """
-    return tf.image.adjust_brightness(image, delta=max_delta)
+    bright = tf.random.uniform((), -max_delta, max_delta)
+    return tf.image.adjust_brightness(image, delta=bright)
