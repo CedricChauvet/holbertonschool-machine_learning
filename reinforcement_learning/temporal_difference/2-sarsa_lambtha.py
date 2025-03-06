37c3869bbc072e26af7d9fb84bb5f358c0a671c5
CedricChauvet <chauvetc.ujf@gmail.com>
2025-03-06 11:30:11 +0100

    fixing epsilon

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -63,13 +63,14 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
                 break
             
             # increment steps counter
-            i += 1
-
+        
         # Decay epsilon after each episode
     
         # Exploration rate decay
         epsilon = (min_epsilon + (initial_epsilon - min_epsilon) *
                    np.exp(-epsilon_decay * i))    
+        i += 1
+
     return Q
 
 
