b6f8b72473be7a1a80052ccaf4173dad1e49c62e
CedricChauvet <chauvetc.ujf@gmail.com>
2025-03-07 11:13:49 +0100

    new version

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -40,9 +40,9 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
                 delta = reward - Q[state, action]
 
             # met a jour l'egibility trace et la table des Q
+            E *= gamma * lambtha
             E[state, action] += 1
             Q += alpha * delta * E
-            E *= gamma * lambtha
             
             if not (done or truncated):
                 state, action = next_state, next_action
