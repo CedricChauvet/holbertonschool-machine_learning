2cb0310de4dea147dfdb9654c88189de16f78d67
CedricChauvet <chauvetc.ujf@gmail.com>
2025-03-07 10:58:15 +0100

    new version

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -25,12 +25,12 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
         steps = 0
         done = truncated = False
         
-        while not (done or truncated):
+        while not (done or truncated or steps >= max_steps):
             steps += 1
-            if steps >= max_steps:
-                truncated = True
                 
             next_state, reward, done, truncated, _ = env.step(action)
+
+            # compute next action if game is over, no moves possible
             next_action = get_action(next_state, Q, epsilon) if not (done or truncated) else 0
             
             # Calcul optimisé
