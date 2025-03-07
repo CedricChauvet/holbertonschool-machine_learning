4bfe3345f12a1dbaaa313f6254fa25b75f0a64e3
CedricChauvet <chauvetc.ujf@gmail.com>
2025-03-07 15:19:49 +0100

    new version

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -4,57 +4,62 @@ Improved version of SARSA(λ)
 """
 import numpy as np
 
+
 def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
-                 gamma=0.99, initial_epsilon=1.0, min_epsilon=0.1, epsilon_decay=0.05):
+                  gamma=0.99, initial_epsilon=1.0, min_epsilon=0.1,
+                  epsilon_decay=0.05):
     """
     Performs SARSA(λ) reinforcement learning algorithm.
     """
     # Validation
     if not (0 <= lambtha <= 1):
         raise ValueError("lambtha must be between 0 and 1")
-        
+
     n_states, n_actions = Q.shape
     epsilon = initial_epsilon
     E = np.zeros((n_states, n_actions))
-    
+
     for episode in range(episodes):
         E.fill(0)
         state = env.reset()[0]
         action = get_action(state, Q, epsilon)
-        
         steps = 0
         done = truncated = False
-        
+
         while not (done or truncated or steps >= max_steps):
             steps += 1
-                
+
             next_state, reward, done, truncated, _ = env.step(action)
 
             # compute next action if game is over, no moves possible
-            next_action = get_action(next_state, Q, epsilon) if not (done or truncated) else 0
-            
+            next_action = get_action(next_state, Q, epsilon)  \
+                if not (done or truncated) else 0
+
             # Calcul optimisé
             if not (done or truncated or steps >= max_steps):
-                delta = reward + (gamma * Q[next_state, next_action]) - Q[state, action]
-            else :
+                delta = reward + (gamma * Q[next_state, next_action]) \
+                    - Q[state, action]
+            else:
                 delta = reward - Q[state, action]
 
             # update egibility trace and Q table
             E[state, action] += 1
             E *= gamma * lambtha
             Q += alpha * delta * E
-            
-            
+
             if not (done or truncated or steps >= max_steps):
                 state, action = next_state, next_action
             else:
                 break
-        
+
         # Décroissance linéaire de epsilon
-        # epsilon = max(min_epsilon, initial_epsilon - (initial_epsilon - min_epsilon) * (episode / episodes))
+        # epsilon = max(min_epsilon, initial_epsilon -
+        # (initial_epsilon - min_epsilon) * (episode / episodes))
         # decroissance exponentielle de epsilon
-        epsilon = min_epsilon + (initial_epsilon - min_epsilon) * np.exp(-epsilon_decay * episode)
+        epsilon = min_epsilon + (initial_epsilon - min_epsilon) \
+            * np.exp(-epsilon_decay * episode)
     return Q
+
 
 def get_action(state, Q, epsilon):
     """Choose action using epsilon-greedy policy"""
